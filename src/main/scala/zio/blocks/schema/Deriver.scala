a58f92d32efa68eaf9a80be877d29ae91d51e6b8
John A. De Goes <john@degoes.net>
2025-02-01 15:57:47 -0500

    cleanups

--- src/main/scala/zio/blocks/schema/Deriver.scala
+++ src/main/scala/zio/blocks/schema/Deriver.scala
@@ -12,32 +12,30 @@ trait HasDerivation[F[_, _], TC[_]] {
 }
 
 trait Deriver[TC[_]] { self =>
-  type F[T, A] = Derivation[TC, T, A]
+  def derivePrimitive[F[_, _], A](prim: Reflect.Primitive[F, A]): TC[A]
 
-  def derivePrimitive[A](prim: Reflect.Primitive[F, A]): TC[A]
-
-  def deriveRecord[A](
+  def deriveRecord[F[_, _], A](
     fields: List[Term[F, A, ?]],
     typeName: TypeName[A],
     doc: Doc,
     modifiers: List[Modifier.Record]
   )(implicit F: HasBinding[F]): TC[A]
 
-  def deriveVariant[A](
+  def deriveVariant[F[_, _], A](
     cases: List[Term[F, A, ?]],
     typeName: TypeName[A],
     doc: Doc,
     modifiers: List[Modifier.Variant]
   )(implicit F: HasBinding[F]): TC[A]
 
-  def deriveSeq[C[_], A](
+  def deriveSeq[F[_, _], C[_], A](
     element: Reflect[F, A],
     typeName: TypeName[C[A]],
     doc: Doc,
     modifiers: List[Modifier.Seq]
   )(implicit F: HasBinding[F]): TC[C[A]]
 
-  def deriveMap[M[_, _], K, V](
+  def deriveMap[F[_, _], M[_, _], K, V](
     key: Reflect[F, K],
     value: Reflect[F, V],
     typeName: TypeName[M[K, V]],
--- src/main/scala/zio/blocks/schema/Doc.scala
+++ src/main/scala/zio/blocks/schema/Doc.scala
@@ -2,11 +2,38 @@ package zio.blocks.schema
 
 sealed trait Doc {
   def +(that: Doc): Doc = Doc.Concat(this, that)
+
+  def flatten: List[Doc.Leaf]
+
+  override lazy val hashCode: Int = flatten.hashCode
+
+  override def equals(that: Any): Boolean = that match {
+    case that: Doc => this.flatten == that.flatten
+    case _         => false
+  }
 }
 object Doc {
-  case object Empty                              extends Doc
-  final case class Text(value: String)           extends Doc
-  final case class Concat(left: Doc, right: Doc) extends Doc
+  sealed trait Leaf extends Doc
+
+  case object Empty extends Leaf {
+    def flatten: List[Leaf] = Nil
+
+    override def equals(that: Any): Boolean = that match {
+      case that: Empty.type => true
+      case _                => false
+    }
+  }
+  final case class Text(value: String) extends Leaf {
+    def flatten: List[Leaf] = List(this)
+
+    override def equals(that: Any): Boolean = that match {
+      case that: Text => this.value == that.value
+      case _          => false
+    }
+  }
+  final case class Concat(left: Doc, right: Doc) extends Doc {
+    lazy val flatten: List[Leaf] = left.flatten ++ right.flatten
+  }
 
   def apply(value: String): Doc = Text(value)
 }
