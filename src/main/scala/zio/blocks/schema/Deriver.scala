f0d9cffd214dc38f0c9d2f2b5d4a291aaf25b2ad
John A. De Goes <john@degoes.net>
2025-02-01 19:24:32 -0500

    cleanups

--- src/main/scala/zio/blocks/schema/Deriver.scala
+++ src/main/scala/zio/blocks/schema/Deriver.scala
@@ -2,16 +2,18 @@ package zio.blocks.schema
 
 import zio.blocks.schema.binding.{BindingType, HasBinding}
 
-final case class Derivation[TC[_], T, A](derivation: TC[A], summoned: Option[TC[A]]) {
-  def preferSummoned: Derivation[TC, T, A] = copy(derivation = summoned.getOrElse(derivation))
+final case class Derivation[TC[_], A](derivation: TC[A], summoned: Option[TC[A]]) {
+  def preferSummoned: Derivation[TC, A] = copy(derivation = summoned.getOrElse(derivation))
 }
 object Derivation {}
 
 trait HasDerivation[F[_, _], TC[_]] {
-  def derivation[T, A](fa: F[T, A]): Derivation[TC, T, A]
+  def derivation[T, A](fa: F[T, A]): Derivation[TC, A]
 }
 
 trait Deriver[TC[_]] { self =>
+  type HasDerivation[F[_, _]] = zio.blocks.schema.HasDerivation[F, TC]
+
   def derivePrimitive[F[_, _], A](prim: Reflect.Primitive[F, A]): TC[A]
 
   def deriveRecord[F[_, _], A](
@@ -26,14 +28,14 @@ trait Deriver[TC[_]] { self =>
     typeName: TypeName[A],
     doc: Doc,
     modifiers: List[Modifier.Variant]
-  )(implicit F: HasBinding[F]): TC[A]
+  )(implicit B: HasBinding[F], D: HasDerivation[F]): TC[A]
 
   def deriveSeq[F[_, _], C[_], A](
     element: Reflect[F, A],
     typeName: TypeName[C[A]],
     doc: Doc,
     modifiers: List[Modifier.Seq]
-  )(implicit F: HasBinding[F]): TC[C[A]]
+  )(implicit B: HasBinding[F], D: HasDerivation[F]): TC[C[A]]
 
   def deriveMap[F[_, _], M[_, _], K, V](
     key: Reflect[F, K],
@@ -41,7 +43,7 @@ trait Deriver[TC[_]] { self =>
     typeName: TypeName[M[K, V]],
     doc: Doc,
     modifiers: List[Modifier.Map]
-  )(implicit F: HasBinding[F]): TC[M[K, V]]
+  )(implicit B: HasBinding[F], D: HasDerivation[F]): TC[M[K, V]]
 
   def preferSummoned: Deriver[TC] = ???
 }
