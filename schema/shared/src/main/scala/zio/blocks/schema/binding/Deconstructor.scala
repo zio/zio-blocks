a9a5705887d4dd1b1bd4c70c18e4c5cedf312af2
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-04-15 11:16:17 +0200

    Add missing return type for public methods

--- schema/shared/src/main/scala/zio/blocks/schema/binding/Deconstructor.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/binding/Deconstructor.scala
@@ -50,77 +50,77 @@ object Deconstructor {
 
   val boolean: Deconstructor[Boolean] =
     new Deconstructor[Boolean] {
-      def usedRegisters = RegisterOffset(booleans = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(booleans = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: Boolean): Unit = out.setBoolean(baseOffset, 0, in)
     }
 
   val byte: Deconstructor[Byte] =
     new Deconstructor[Byte] {
-      def usedRegisters = RegisterOffset(bytes = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(bytes = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: Byte): Unit = out.setByte(baseOffset, 0, in)
     }
 
   val short: Deconstructor[Short] =
     new Deconstructor[Short] {
-      def usedRegisters = RegisterOffset(shorts = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(shorts = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: Short): Unit = out.setShort(baseOffset, 0, in)
     }
 
   val int: Deconstructor[Int] =
     new Deconstructor[Int] {
-      def usedRegisters = RegisterOffset(ints = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(ints = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: Int): Unit = out.setInt(baseOffset, 0, in)
     }
 
   val long: Deconstructor[Long] =
     new Deconstructor[Long] {
-      def usedRegisters = RegisterOffset(longs = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(longs = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: Long): Unit = out.setLong(baseOffset, 0, in)
     }
 
   val float: Deconstructor[Float] =
     new Deconstructor[Float] {
-      def usedRegisters = RegisterOffset(floats = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(floats = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: Float): Unit = out.setFloat(baseOffset, 0, in)
     }
 
   val double: Deconstructor[Double] =
     new Deconstructor[Double] {
-      def usedRegisters = RegisterOffset(doubles = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(doubles = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: Double): Unit = out.setDouble(baseOffset, 0, in)
     }
 
   val char: Deconstructor[Char] =
     new Deconstructor[Char] {
-      def usedRegisters = RegisterOffset(chars = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(chars = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: Char): Unit = out.setChar(baseOffset, 0, in)
     }
 
   val string: Deconstructor[String] =
     new Deconstructor[String] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: String): Unit = out.setObject(baseOffset, 0, in)
     }
 
   val bigInt: Deconstructor[BigInt] =
     new Deconstructor[BigInt] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: BigInt): Unit = out.setObject(baseOffset, 0, in)
     }
 
   val bigDecimal: Deconstructor[BigDecimal] =
     new Deconstructor[BigDecimal] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: BigDecimal): Unit =
         out.setObject(baseOffset, 0, in)
@@ -128,7 +128,7 @@ object Deconstructor {
 
   val dayOfWeek: Deconstructor[java.time.DayOfWeek] =
     new Deconstructor[java.time.DayOfWeek] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.DayOfWeek): Unit =
         out.setObject(baseOffset, 0, in)
@@ -136,7 +136,7 @@ object Deconstructor {
 
   val duration: Deconstructor[java.time.Duration] =
     new Deconstructor[java.time.Duration] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.Duration): Unit =
         out.setObject(baseOffset, 0, in)
@@ -144,7 +144,7 @@ object Deconstructor {
 
   val instant: Deconstructor[java.time.Instant] =
     new Deconstructor[java.time.Instant] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.Instant): Unit =
         out.setObject(baseOffset, 0, in)
@@ -152,7 +152,7 @@ object Deconstructor {
 
   val localDate: Deconstructor[java.time.LocalDate] =
     new Deconstructor[java.time.LocalDate] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.LocalDate): Unit =
         out.setObject(baseOffset, 0, in)
@@ -160,7 +160,7 @@ object Deconstructor {
 
   val localDateTime: Deconstructor[java.time.LocalDateTime] =
     new Deconstructor[java.time.LocalDateTime] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.LocalDateTime): Unit =
         out.setObject(baseOffset, 0, in)
@@ -168,7 +168,7 @@ object Deconstructor {
 
   val localTime: Deconstructor[java.time.LocalTime] =
     new Deconstructor[java.time.LocalTime] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.LocalTime): Unit =
         out.setObject(baseOffset, 0, in)
@@ -176,7 +176,7 @@ object Deconstructor {
 
   val month: Deconstructor[java.time.Month] =
     new Deconstructor[java.time.Month] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.Month): Unit =
         out.setObject(baseOffset, 0, in)
@@ -184,7 +184,7 @@ object Deconstructor {
 
   val monthDay: Deconstructor[java.time.MonthDay] =
     new Deconstructor[java.time.MonthDay] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.MonthDay): Unit =
         out.setObject(baseOffset, 0, in)
@@ -192,7 +192,7 @@ object Deconstructor {
 
   val offsetDateTime: Deconstructor[java.time.OffsetDateTime] =
     new Deconstructor[java.time.OffsetDateTime] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.OffsetDateTime): Unit =
         out.setObject(baseOffset, 0, in)
@@ -200,7 +200,7 @@ object Deconstructor {
 
   val offsetTime: Deconstructor[java.time.OffsetTime] =
     new Deconstructor[java.time.OffsetTime] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.OffsetTime): Unit =
         out.setObject(baseOffset, 0, in)
@@ -208,7 +208,7 @@ object Deconstructor {
 
   val period: Deconstructor[java.time.Period] =
     new Deconstructor[java.time.Period] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.Period): Unit =
         out.setObject(baseOffset, 0, in)
@@ -216,7 +216,7 @@ object Deconstructor {
 
   val year: Deconstructor[java.time.Year] =
     new Deconstructor[java.time.Year] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.Year): Unit =
         out.setObject(baseOffset, 0, in)
@@ -224,7 +224,7 @@ object Deconstructor {
 
   val yearMonth: Deconstructor[java.time.YearMonth] =
     new Deconstructor[java.time.YearMonth] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.YearMonth): Unit =
         out.setObject(baseOffset, 0, in)
@@ -232,7 +232,7 @@ object Deconstructor {
 
   val zoneId: Deconstructor[java.time.ZoneId] =
     new Deconstructor[java.time.ZoneId] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.ZoneId): Unit =
         out.setObject(baseOffset, 0, in)
@@ -240,7 +240,7 @@ object Deconstructor {
 
   val zoneOffset: Deconstructor[java.time.ZoneOffset] =
     new Deconstructor[java.time.ZoneOffset] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.ZoneOffset): Unit =
         out.setObject(baseOffset, 0, in)
@@ -248,7 +248,7 @@ object Deconstructor {
 
   val zonedDateTime: Deconstructor[java.time.ZonedDateTime] =
     new Deconstructor[java.time.ZonedDateTime] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.time.ZonedDateTime): Unit =
         out.setObject(baseOffset, 0, in)
@@ -256,7 +256,7 @@ object Deconstructor {
 
   val currency: Deconstructor[java.util.Currency] =
     new Deconstructor[java.util.Currency] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.util.Currency): Unit =
         out.setObject(baseOffset, 0, in)
@@ -264,7 +264,7 @@ object Deconstructor {
 
   val uuid: Deconstructor[java.util.UUID] =
     new Deconstructor[java.util.UUID] {
-      def usedRegisters = RegisterOffset(objects = 1)
+      def usedRegisters: RegisterOffset = RegisterOffset(objects = 1)
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: java.util.UUID): Unit =
         out.setObject(baseOffset, 0, in)
@@ -272,7 +272,7 @@ object Deconstructor {
 
   val none: Deconstructor[None.type] =
     new Deconstructor[None.type] {
-      def usedRegisters = RegisterOffset.Zero
+      def usedRegisters: RegisterOffset = RegisterOffset.Zero
 
       def deconstruct(out: Registers, baseOffset: RegisterOffset, in: None.type): Unit = ()
     }
