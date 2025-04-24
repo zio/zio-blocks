79af813336affb9b20a8ebe2781f2267a01de245
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-04-24 13:14:32 +0200

    Generate constructors and deconstructors

--- schema/shared/src/main/scala-2/zio/blocks/schema/SchemaVersionSpecific.scala
+++ schema/shared/src/main/scala-2/zio/blocks/schema/SchemaVersionSpecific.scala
@@ -14,7 +14,6 @@ object SchemaVersionSpecific {
     import c.universe._
     import c.internal._
     import zio.blocks.schema.binding.RegisterOffset
-    import zio.blocks.schema.binding.RegisterOffset.RegisterOffset
 
     def fail(msg: String): Nothing = c.abort(c.enclosingPosition, msg)
 
@@ -42,11 +41,12 @@ object SchemaVersionSpecific {
     val tpe = weakTypeOf[A].dealias
     if (isNonAbstractScalaClass(tpe)) {
       case class FieldInfo(
+        symbol: Symbol,
         name: String,
         tpe: Type,
-        getter: Symbol,
         defaultValue: Option[Tree],
-        offset: RegisterOffset
+        const: Tree,
+        deconst: Tree
       )
 
       val tpeTypeSymbol = tpe.typeSymbol
@@ -80,41 +80,73 @@ object SchemaVersionSpecific {
         i += 1
         val symbol = param.asTerm
         val name   = NameTransformer.decode(symbol.name.toString)
-        FieldInfo(
-          name = name,
-          offset = offset,
-          tpe = {
-            var fTpe = symbol.typeSignature.dealias
-            if (tpeTypeArgs.nonEmpty) fTpe = fTpe.substituteTypes(tpeTypeParams, tpeTypeArgs)
-            offset = RegisterOffset.add(
-              offset,
-              if (fTpe =:= typeOf[Boolean] || fTpe =:= typeOf[java.lang.Boolean]) RegisterOffset(booleans = 1)
-              else if (fTpe =:= typeOf[Byte] || fTpe =:= typeOf[java.lang.Byte]) RegisterOffset(bytes = 1)
-              else if (fTpe =:= typeOf[Char] || fTpe =:= typeOf[java.lang.Character]) RegisterOffset(chars = 1)
-              else if (fTpe =:= typeOf[Short] || fTpe =:= typeOf[java.lang.Short]) RegisterOffset(shorts = 1)
-              else if (fTpe =:= typeOf[Float] || fTpe =:= typeOf[java.lang.Float]) RegisterOffset(floats = 1)
-              else if (fTpe =:= typeOf[Int] || fTpe =:= typeOf[java.lang.Integer]) RegisterOffset(ints = 1)
-              else if (fTpe =:= typeOf[Double] || fTpe =:= typeOf[java.lang.Double]) RegisterOffset(doubles = 1)
-              else if (fTpe =:= typeOf[Long] || fTpe =:= typeOf[java.lang.Long]) RegisterOffset(longs = 1)
-              else RegisterOffset(objects = 1)
-            )
-            fTpe
-          },
-          getter =
-            getters.getOrElse(name, fail(s"Cannot find '$name' parameter of '$tpe' in the primary constructor.")),
-          defaultValue = {
-            if (symbol.isParamWithDefault) Some(q"${comp.asModule}.${TermName("$lessinit$greater$default$" + i)}")
-            else None
-          }
-        )
+        var fTpe   = symbol.typeSignature.dealias
+        if (tpeTypeArgs.nonEmpty) fTpe = fTpe.substituteTypes(tpeTypeParams, tpeTypeArgs)
+        val defaultValue =
+          if (symbol.isParamWithDefault) Some(q"${comp.asModule}.${TermName("$lessinit$greater$default$" + i)}")
+          else None
+        val getter =
+          getters.getOrElse(name, fail(s"Cannot find '$name' parameter of '$tpe' in the primary constructor."))
+        var offsetInc = RegisterOffset.Zero
+        var const     = q""
+        var deconst   = q""
+        if (fTpe =:= typeOf[Boolean] || fTpe =:= typeOf[java.lang.Boolean]) {
+          offsetInc = RegisterOffset(booleans = 1)
+          val bytes = RegisterOffset.getBytes(offset)
+          const = q"in.getBoolean(baseOffset, $bytes)"
+          deconst = q"out.setBoolean(baseOffset, $bytes, in.$getter)"
+        } else if (fTpe =:= typeOf[Byte] || fTpe =:= typeOf[java.lang.Byte]) {
+          offsetInc = RegisterOffset(bytes = 1)
+          val bytes = RegisterOffset.getBytes(offset)
+          const = q"in.getByte(baseOffset, $bytes)"
+          deconst = q"out.setByte(baseOffset, $bytes, in.$getter)"
+        } else if (fTpe =:= typeOf[Char] || fTpe =:= typeOf[java.lang.Character]) {
+          offsetInc = RegisterOffset(chars = 1)
+          val bytes = RegisterOffset.getBytes(offset)
+          const = q"in.getChar(baseOffset, $bytes)"
+          deconst = q"out.setChar(baseOffset, $bytes, in.$getter)"
+        } else if (fTpe =:= typeOf[Short] || fTpe =:= typeOf[java.lang.Short]) {
+          offsetInc = RegisterOffset(shorts = 1)
+          val bytes = RegisterOffset.getBytes(offset)
+          const = q"in.getShort(baseOffset, $bytes)"
+          deconst = q"out.setShort(baseOffset, $bytes, in.$getter)"
+        } else if (fTpe =:= typeOf[Float] || fTpe =:= typeOf[java.lang.Float]) {
+          offsetInc = RegisterOffset(floats = 1)
+          val bytes = RegisterOffset.getBytes(offset)
+          const = q"in.getFloat(baseOffset, $bytes)"
+          deconst = q"out.setFloat(baseOffset, $bytes, in.$getter)"
+        } else if (fTpe =:= typeOf[Int] || fTpe =:= typeOf[java.lang.Integer]) {
+          offsetInc = RegisterOffset(ints = 1)
+          val bytes = RegisterOffset.getBytes(offset)
+          const = q"in.getInt(baseOffset, $bytes)"
+          deconst = q"out.setInt(baseOffset, $bytes, in.$getter)"
+        } else if (fTpe =:= typeOf[Double] || fTpe =:= typeOf[java.lang.Double]) {
+          offsetInc = RegisterOffset(doubles = 1)
+          val bytes = RegisterOffset.getBytes(offset)
+          const = q"in.getDouble(baseOffset, $bytes)"
+          deconst = q"out.setDouble(baseOffset, $bytes, in.$getter)"
+        } else if (fTpe =:= typeOf[Long] || fTpe =:= typeOf[java.lang.Long]) {
+          offsetInc = RegisterOffset(longs = 1)
+          val bytes = RegisterOffset.getBytes(offset)
+          const = q"in.getLong(baseOffset, $bytes)"
+          deconst = q"out.setLong(baseOffset, $bytes, in.$getter)"
+        } else {
+          offsetInc = RegisterOffset(objects = 1)
+          val objects = RegisterOffset.getObjects(offset)
+          const = q"in.getObject(baseOffset, $objects).asInstanceOf[$fTpe]"
+          deconst = q"out.setObject(baseOffset, $objects, in.$getter)"
+        }
+        offset = RegisterOffset.add(offset, offsetInc)
+        FieldInfo(symbol, name, fTpe, defaultValue, const, deconst)
       })
       val fields = fieldInfos.flatMap(_.map { fieldInfo =>
         fieldInfo.defaultValue.fold(q"Schema[${fieldInfo.tpe}].reflect.asTerm(${fieldInfo.name})") { defaultValue =>
           q"Schema[${fieldInfo.tpe}].reflect.defaultValue($defaultValue).asTerm(${fieldInfo.name})"
         }
       })
-      // TODO: use `fieldInfos` to generate remaining `Reflect.Record.fields` and `Reflect.Record.recordBinding`
-      c.Expr[Schema[A]](
+      val const   = q"new $tpe(...${fieldInfos.map(_.map(fieldInfo => q"${fieldInfo.symbol} = ${fieldInfo.const}"))})"
+      val deconst = fieldInfos.flatMap(_.map(_.deconst))
+      val schema =
         q"""{
               import _root_.zio.blocks.schema._
               import _root_.zio.blocks.schema.binding._
@@ -134,12 +166,14 @@ object SchemaVersionSpecific {
                     constructor = new Constructor[$tpe] {
                       def usedRegisters: RegisterOffset = $offset
 
-                      def construct(in: Registers, baseOffset: RegisterOffset): $tpe = ???
+                      def construct(in: Registers, baseOffset: RegisterOffset): $tpe = $const
                     },
                     deconstructor = new Deconstructor[$tpe] {
                       def usedRegisters: RegisterOffset = $offset
 
-                      def deconstruct(out: Registers, baseOffset: RegisterOffset, in: $tpe): Unit = ???
+                      def deconstruct(out: Registers, baseOffset: RegisterOffset, in: $tpe): Unit = {
+                        ..$deconst
+                      }
                     },
                     defaultValue = _root_.scala.None,
                     examples = _root_.scala.Nil
@@ -149,7 +183,8 @@ object SchemaVersionSpecific {
                 )
               )
             }"""
-      )
+      // c.info(c.enclosingPosition, s"Generated schema for type '$tpe':\n${showCode(schema)}", force = true)
+      c.Expr[Schema[A]](schema)
     } else fail(s"Cannot derive '${typeOf[Schema[_]]}' for '$tpe'.")
   }
 }
