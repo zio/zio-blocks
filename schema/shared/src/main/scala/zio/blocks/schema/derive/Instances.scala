e34b166e63912b2da58e50201f870966f2e5c867
John De Goes <john@degoes.net>
2025-05-01 11:28:32 -0400

    more

--- schema/shared/src/main/scala/zio/blocks/schema/derive/Instances.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/derive/Instances.scala
@@ -1,4 +1,4 @@
-package zio.blocks.schema
+package zio.blocks.schema.derive
 
 final case class Instances[TC[_], A](automatic: Lazy[TC[A]], custom: Option[Lazy[TC[A]]]) {
   def derivation: Lazy[TC[A]] = custom.getOrElse(automatic)
