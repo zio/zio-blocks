bd6107ee3466cd466b305100905497dc6a34a77c
John De Goes <john@degoes.net>
2025-05-01 11:28:32 -0400

    more

--- schema/shared/src/main/scala/zio/blocks/schema/derive/DeriveOverride.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/derive/DeriveOverride.scala
@@ -1,3 +1,3 @@
-package zio.blocks.schema
+package zio.blocks.schema.derive
 
 final case class DeriveOverride[TC[_], S, A](optic: Optic.Bound[S, A], tc: TC[A])
--- schema/shared/src/main/scala/zio/blocks/schema/derive/HasDerivation.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/derive/HasDerivation.scala
@@ -1,4 +1,4 @@
-package zio.blocks.schema
+package zio.blocks.schema.derive
 
 trait HasDerivation[F[_, _], TC[_]] {
   def instances[T, A](fa: F[T, A]): Instances[TC, A]
