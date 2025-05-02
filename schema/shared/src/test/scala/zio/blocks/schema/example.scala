0f3a404bbe917f7e11339d6a1a2fadf6fb84e6ad
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-05-02 19:56:59 +0200

    Add `CompanionOptics` macro (#44)

--- schema/shared/src/test/scala/zio/blocks/schema/example.scala
+++ schema/shared/src/test/scala/zio/blocks/schema/example.scala
@@ -1,101 +1,25 @@
 package zio.blocks.schema
 
-import zio.blocks.schema.binding.RegisterOffset.RegisterOffset
-import zio.blocks.schema.binding.Binding._
-import zio.blocks.schema.binding._
+object example {
+  case class Person(id: Long, name: String, age: Int, address: String, childrenAges: List[Int])
 
-object Main {
-  final case class Person(id: java.util.UUID, name: String, age: Int, address: String, childrenAges: List[Int])
-
-  object Person {
-    // Proposed macros:
-    //
-    // implicit val personSchema: Schema[Person] = deriveSchema[Person]
-    //
-    // val name          = field[Person](_.name)
-    // val age           = field[Person](_.age)
-    // val address       = field[Person](_.address)
-    // val childrenAges  = field[Person](_.childrenAges)
-    // val childrenAges2 = field[Person](_.childrenAges).list // Traversal
-    // val left = caseOf[Either[String, Int], Left[String, Int]]
-
-    val constructor: Constructor[Person] =
-      new Constructor[Person] {
-        def usedRegisters: RegisterOffset = RegisterOffset(ints = 1, objects = 3)
-
-        def construct(in: Registers, baseOffset: RegisterOffset): Person = {
-          val id      = in.getObject(baseOffset, 0).asInstanceOf[java.util.UUID]
-          val name    = in.getObject(baseOffset, 1).asInstanceOf[String]
-          val age     = in.getInt(baseOffset, 0)
-          val address = in.getObject(baseOffset, 2).asInstanceOf[String]
-          val ages    = in.getObject(baseOffset, 3).asInstanceOf[List[Int]]
-
-          Person(id, name, age, address, ages)
-        }
-      }
-
-    val deconstructor: Deconstructor[Person] =
-      new Deconstructor[Person] {
-        def usedRegisters: RegisterOffset = RegisterOffset(ints = 1, objects = 4)
-
-        def deconstruct(out: Registers, baseOffset: RegisterOffset, in: Person): Unit = {
-          out.setObject(baseOffset, 0, in.id)
-          out.setObject(baseOffset, 1, in.name)
-          out.setInt(baseOffset, 0, in.age)
-          out.setObject(baseOffset, 2, in.address)
-          out.setObject(baseOffset, 3, in.childrenAges)
-        }
-      }
-
-    val personRecord: Reflect.Record.Bound[Person] =
-      Reflect.Record(
-        List(
-          Reflect.uuid.asTerm("id"),
-          Reflect.string.asTerm("name"),
-          Reflect.int.asTerm("age"),
-          Reflect.string.asTerm("address"),
-          Reflect.list(Reflect.int).asTerm("childrenAges")
-        ),
-        TypeName(Namespace(List("example"), Nil), "Person"),
-        Binding.Record(constructor, deconstructor),
-        Doc.Empty,
-        Nil
-      )
-
-    val id: Lens[Person, java.util.UUID] =
-      Lens(personRecord, personRecord.fields(0).asInstanceOf[Term.Bound[Person, java.util.UUID]])
-    val name: Lens[Person, String] =
-      Lens(personRecord, personRecord.fields(1).asInstanceOf[Term.Bound[Person, String]])
-    val age: Lens[Person, Int] =
-      Lens(personRecord, personRecord.fields(2).asInstanceOf[Term.Bound[Person, Int]])
-    val address: Lens[Person, String] =
-      Lens(personRecord, personRecord.fields(3).asInstanceOf[Term.Bound[Person, String]])
-    val childrenAges: Traversal[Person, Int] =
-      Lens(personRecord, personRecord.fields(4).asInstanceOf[Term.Bound[Person, List[Int]]]).listValues
+  object Person extends CompanionOptics[Person] {
+    implicit val schema: Schema[Person]      = Schema.derived
+    val id: Lens[Person, Long]               = field(_.id)
+    val name: Lens[Person, String]           = field(_.name)
+    val age: Lens[Person, Int]               = field(_.age)
+    val address: Lens[Person, String]        = field(_.address)
+    val childrenAges: Traversal[Person, Int] = field(_.childrenAges).listValues
   }
 
-  import Person._
-
   def main(args: Array[String]): Unit = {
-    personRecord.registers.foreach(println)
-    personRecord.fields.foreach(println)
+    val person = Person(12345678901L, "John", 30, "123 Main St", List(5, 7, 9))
 
-    val person = Person(new java.util.UUID(1L, 1L), "John", 30, "123 Main St", List(5, 7, 9))
-
-    println("id:      " + Person.id.get(person))
-    println("name:    " + Person.name.get(person))
-    println("age:     " + Person.age.get(person))
-    println("address: " + Person.address.get(person))
-
-    // (a)(b)(c)
-    // a > b > c
-
-    val newPerson = Person.name.replace(person, "Jane")
-
-    println("newPerson: " + newPerson)
-
-    val newPerson2 = Person.childrenAges.modify(person, _ + 1)
-
-    println("newPerson2: " + newPerson2)
+    println("id:         " + Person.id.get(person))
+    println("name:       " + Person.name.get(person))
+    println("age:        " + Person.age.get(person))
+    println("address:    " + Person.address.get(person))
+    println("newPerson:  " + Person.name.replace(person, "Jane"))
+    println("newPerson2: " + Person.childrenAges.modify(person, _ + 1))
   }
 }
