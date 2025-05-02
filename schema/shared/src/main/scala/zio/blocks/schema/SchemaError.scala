8d07f0081c8d121115213ccc18199a5a5bb1c615
John De Goes <john@degoes.net>
2025-05-02 17:20:14 -0400

    CodecError => SchemaError

--- schema/shared/src/main/scala/zio/blocks/schema/SchemaError.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/SchemaError.scala
@@ -1,16 +1,16 @@
-package zio.blocks.schema.codec
+package zio.blocks.schema
 
 import zio.blocks.schema._
 
 import scala.util.control.NoStackTrace
 
-sealed trait CodecError extends Exception with NoStackTrace {
+sealed trait SchemaError extends Exception with NoStackTrace {
   def message: String
 
   override def getMessage: String = message
 }
 
-object CodecError {
+object SchemaError {
   // FIXME: Flesh these out
   case class ValidationError[F[_, _], A](
     structure: Reflect[F, ?],
@@ -18,23 +18,24 @@ object CodecError {
     expected: Validation[A],
     actual: A,
     message: String
-  ) extends CodecError
+  ) extends SchemaError
 
   case class MissingField[F[_, _], S, A](structure: Reflect.Record[F, S], field: Term[F, S, A], message: String)
-      extends CodecError
+      extends SchemaError
 
   case class UnknownField[F[_, _], S, A](structure: Reflect.Record[F, S], field: Term[F, S, A], message: String)
-      extends CodecError
+      extends SchemaError
 
-  case class InvalidType[F[_, _], A](structure: Reflect[F, ?], focus: Reflect[F, A], message: String) extends CodecError
+  case class InvalidType[F[_, _], A](structure: Reflect[F, ?], focus: Reflect[F, A], message: String)
+      extends SchemaError
 
   case class MissingCase[F[_, _], S, A](structure: Reflect.Variant[F, S], case0: Term[F, S, A], message: String)
-      extends CodecError
+      extends SchemaError
 
   case class UnknownCase[F[_, _], S, A](structure: Reflect.Variant[F, S], case0: Term[F, S, A], message: String)
-      extends CodecError
+      extends SchemaError
 
-  case class MultipleErrors(errors: ::[CodecError]) extends CodecError {
+  case class MultipleErrors(errors: ::[SchemaError]) extends SchemaError {
     def message: String = errors.map(_.message).mkString("\n")
   }
 }
