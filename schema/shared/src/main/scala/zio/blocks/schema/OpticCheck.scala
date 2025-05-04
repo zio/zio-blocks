d4a7906026cd8d9bc35ca5e8efc6b15987126a12
John De Goes <john@degoes.net>
2025-05-03 20:13:42 -0400

    sketching out error messages for traversal

--- schema/shared/src/main/scala/zio/blocks/schema/OpticCheck.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/OpticCheck.scala
@@ -5,15 +5,25 @@ import scala.util.control.NoStackTrace
 final case class OpticCheck(errors: ::[OpticCheck.Single]) extends Exception with NoStackTrace {
   def ++(other: OpticCheck): OpticCheck = OpticCheck(::(errors.head, errors.tail ++ other.errors))
 
+  override def getMessage: String = message
+
   def isWarning: Boolean = errors.exists(_.isWarning)
 
   def isError: Boolean = errors.exists(_.isError)
 
   def message: String = errors.map(_.message).mkString("\n")
 
-  override def getMessage: String = message
+  def shift(prefix: DynamicOptic): OpticCheck =
+    OpticCheck(errors.map(_.shift(prefix)).asInstanceOf[::[OpticCheck.Single]])
+
 }
 object OpticCheck {
+  def emptyMap(full: DynamicOptic, prefix: DynamicOptic, actualValue: Any): OpticCheck =
+    OpticCheck(::(EmptyMap(full, prefix, actualValue), Nil))
+
+  def emptySequence(full: DynamicOptic, prefix: DynamicOptic, actualValue: Any): OpticCheck =
+    OpticCheck(::(EmptySequence(full, prefix, actualValue), Nil))
+
   def unexpectedCase(
     expectedCase: String,
     actualCase: String,
@@ -34,6 +44,8 @@ object OpticCheck {
     }
 
     def isError: Boolean = !isWarning
+
+    def shift(prefix0: DynamicOptic): Single
   }
 
   sealed trait Error   extends Single
@@ -48,5 +60,24 @@ object OpticCheck {
   ) extends Error {
     def message: String =
       s"During attempted access at $full, encountered an unexpected case at $prefix: Expected $expectedCase, but got $actualCase ($actualValue)"
+
+    def shift(prefix0: DynamicOptic): UnexpectedCase =
+      copy(full = prefix0(full), prefix = prefix0(prefix))
+  }
+
+  final case class EmptySequence(full: DynamicOptic, prefix: DynamicOptic, actualValue: Any) extends Warning {
+    def message: String =
+      s"During attempted access at $full, encountered an empty sequence at $prefix: $actualValue"
+
+    def shift(prefix0: DynamicOptic): EmptySequence =
+      copy(full = prefix0(full), prefix = prefix0(prefix))
+  }
+
+  final case class EmptyMap(full: DynamicOptic, prefix: DynamicOptic, actualValue: Any) extends Warning {
+    def message: String =
+      s"During attempted access at $full, encountered an empty map at $prefix: $actualValue"
+
+    def shift(prefix0: DynamicOptic): EmptyMap =
+      copy(full = prefix0(full), prefix = prefix0(prefix))
   }
 }
