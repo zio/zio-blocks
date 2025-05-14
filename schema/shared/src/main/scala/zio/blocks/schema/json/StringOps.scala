83b3fad0eecfc853947e75e9cd8f549f849473a7
Oto Brglez <otobrglez@gmail.com>
2025-05-14 13:16:22 -0600

    Cleanup and refactoring.

---
+++ schema/shared/src/main/scala/zio/blocks/schema/json/StringOps.scala
@@ -0,0 +1,18 @@
+package zio.blocks.schema.json
+
+object StringOps {
+
+  implicit class StringEx(val self: String) extends AnyVal {
+    final def escape: String = self.flatMap {
+      case '"'          => "\\\""
+      case '\\'         => "\\\\"
+      case '\b'         => "\\b"
+      case '\f'         => "\\f"
+      case '\n'         => "\\n"
+      case '\r'         => "\\r"
+      case '\t'         => "\\t"
+      case c if c < ' ' => "\\u%04x".format(c.toInt)
+      case c            => c.toString
+    }
+  }
+}
