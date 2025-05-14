635574aca5c5d5f3c1eb41559a5e3462ddcddbbe
sviezypan <jaroslav.regec1@gmail.com>
2025-05-14 16:30:40 -0600

    cleanup

--- schema/shared/src/test/scala/zio/blocks/schema/SchemaAspectSpec.scala
+++ schema/shared/src/test/scala/zio/blocks/schema/SchemaAspectSpec.scala
@@ -6,20 +6,24 @@ import zio.test._
 
 object SchemaAspectSpec extends ZIOSpecDefault {
 
+  final case class Person(name: String, age: Int)
+
+  object Person extends CompanionOptics[Person] {
+    implicit val schema: Schema[Person] = Schema.derived
+
+    val nameLens: Lens[Person, String] = field(_.name)
+    val ageLens: Lens[Person, Int]     = field(_.age)
+  }
+  import Person._
+
   def spec: Spec[TestEnvironment with Scope, Any] =
     suite("SchemaAspectSpec")(
       test("identity aspect") {
-        final case class Person(name: String, age: Int)
-        implicit val schema: Schema[Person] = Schema.derived
-
         val updatedSchema = schema @@ SchemaAspect.identity
 
         assert(updatedSchema)(equalTo(schema))
       },
       test("doc aspect") {
-        final case class Person(name: String, age: Int)
-        implicit val schema: Schema[Person] = Schema.derived
-
         val doc = "Person data type"
 
         val updatedSchema = schema @@ SchemaAspect.doc(doc)
@@ -27,9 +31,6 @@ object SchemaAspectSpec extends ZIOSpecDefault {
         assert(updatedSchema.doc)(equalTo(Doc.Text(doc)))
       },
       test("example aspect") {
-        final case class Person(name: String, age: Int)
-        implicit val schema: Schema[Person] = Schema.derived
-
         val p = Person("Jaro", 34)
 
         val updatedSchema = schema @@ SchemaAspect.examples(p)
@@ -37,18 +38,6 @@ object SchemaAspectSpec extends ZIOSpecDefault {
         assert(updatedSchema.examples)(equalTo(Seq(p)))
       },
       test("update doc of a field") {
-        final case class Person(name: String, age: Int)
-
-        object Person extends CompanionOptics[Person] {
-          implicit val schema: Schema[Person] = Schema.derived
-
-          val nameLens: Lens[Person, String] = field(_.name)
-          val ageLens: Lens[Person, Int]     = field(_.age)
-        }
-        import Person._
-
-        val p = Person("Jaro", 34)
-
         val doc = "name of the person"
 
         val updatedSchema = schema @@ (Person.nameLens, SchemaAspect.doc(doc))
