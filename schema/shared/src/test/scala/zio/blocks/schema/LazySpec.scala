94049ec4117b3e0acb121b144e522f027abb7f08
EstebanMarin <estebanmarin002@gmail.com>
2025-05-14 13:25:05 -0600

    LazySpec.scala: Add missing tests for Lazy and LazyRef

--- schema/shared/src/test/scala/zio/blocks/schema/LazySpec.scala
+++ schema/shared/src/test/scala/zio/blocks/schema/LazySpec.scala
@@ -9,15 +9,15 @@ object LazySpec extends ZIOSpecDefault {
     suite("LazySpec")(
       test("has consistent as") {
         assert(Lazy[Int](42).as[String]("42"))(equalTo(Lazy[String]("42")))
-    //   }
       },
-    //   test("catchAll") {
-    //     val lazyValue: Lazy[Int] = Lazy(throw new Exception("error"))
-    //     val lazyCatchAll: Lazy[Int] = lazyValue.catchAll(_ => Lazy(42))
-    //     assert(Lazy(throw new Exception("error")).force)(equalTo(42))
-    //   }
+      test("catchAll") {
+        val lazyValue: Lazy[Int] = Lazy(throw new Exception("error"))
+        val lazyCatchAll: Lazy[Int] = lazyValue.catchAll(_ => Lazy(42))
+        assert(Lazy(throw new Exception("error")).force)(equalTo(42))
+      },
       test("ensuring") {
         val lazyValue: Lazy[Int] = Lazy(42)
+        // TODO test other values [Any]
         val lazyEnsuring: Lazy[Int] = lazyValue.ensuring(Lazy(42))
         assert(lazyEnsuring)(equalTo(Lazy(42)))
       }
