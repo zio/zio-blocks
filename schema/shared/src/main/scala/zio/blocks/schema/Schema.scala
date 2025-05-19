81469259a63bc4cbbde113ea75a40e04bb1b80eb
sviezypan <jaroslav.regec1@gmail.com>
2025-05-19 12:08:34 +0200

    added type bounds to Min and Max

--- schema/shared/src/main/scala/zio/blocks/schema/Schema.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/Schema.scala
@@ -68,7 +68,7 @@ final case class Schema[A](reflect: Reflect.Bound[A]) {
   def updated[B](optic: Optic[A, B])(f: Reflect.Bound[B] => Reflect.Bound[B]): Option[Schema[A]] =
     reflect.updated(optic)(f).map(Schema(_))
 
-  def @@[Min, Max](aspect: SchemaAspect[Min, Max])(implicit ev1: Max <:< A, ev2: A <:< Min): Schema[A] =
+  def @@[Min >: A, Max <: A](aspect: SchemaAspect[Min, Max]): Schema[A] =
     new Schema(reflect.aspect(aspect))
 
   def @@[B](part: Optic[A, B], aspect: SchemaAspect[B, B]) = new Schema(reflect.aspect(part, aspect))
