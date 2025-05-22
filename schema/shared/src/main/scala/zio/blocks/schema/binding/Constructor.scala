c1735d14523a914b2ed16fc3dae086d2eada1eb5
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-05-22 15:02:22 +0200

    Remove unused methods

--- schema/shared/src/main/scala/zio/blocks/schema/binding/Constructor.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/binding/Constructor.scala
@@ -17,23 +17,4 @@ abstract class Constructor[+A] { self =>
    * Constructs a value of type `A` from the registers.
    */
   def construct(in: Registers, baseOffset: RegisterOffset): A
-
-  /**
-   * Maps a function over the constructed value.
-   */
-  final def map[B](f: A => B): Constructor[B] =
-    new Constructor[B] {
-      def usedRegisters: RegisterOffset = self.usedRegisters
-
-      def construct(in: Registers, baseOffset: RegisterOffset): B = f(self.construct(in, baseOffset))
-    }
-}
-
-object Constructor {
-  val unit: Constructor[Unit] =
-    new Constructor[Unit] {
-      def usedRegisters: RegisterOffset = RegisterOffset.Zero
-
-      def construct(in: Registers, baseOffset: RegisterOffset): Unit = ()
-    }
 }
