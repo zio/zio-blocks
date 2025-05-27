5de19b25243071731abb408dfd5669a9b92f6e15
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-05-27 11:21:21 +0200

    More efficient instantiation of schema errors

--- schema/shared/src/main/scala/zio/blocks/schema/SchemaError.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/SchemaError.scala
@@ -1,5 +1,6 @@
 package zio.blocks.schema
 
+import scala.collection.immutable.ArraySeq
 import scala.util.control.NoStackTrace
 
 final case class SchemaError(errors: ::[SchemaError.Single]) extends Exception with NoStackTrace {
@@ -12,16 +13,32 @@ final case class SchemaError(errors: ::[SchemaError.Single]) extends Exception w
 
 object SchemaError {
   private[schema] def invalidType(trace: List[DynamicOptic.Node], expectation: String): SchemaError =
-    new SchemaError(new ::(new InvalidType(DynamicOptic(trace.toVector.reverse), expectation), Nil))
+    new SchemaError(new ::(new InvalidType(toDynamicOptic(trace), expectation), Nil))
 
   private[schema] def missingField(trace: List[DynamicOptic.Node], fieldName: String): SchemaError =
-    new SchemaError(new ::(new MissingField(DynamicOptic(trace.toVector.reverse), fieldName), Nil))
+    new SchemaError(new ::(new MissingField(toDynamicOptic(trace), fieldName), Nil))
 
   private[schema] def duplicatedField(trace: List[DynamicOptic.Node], fieldName: String): SchemaError =
-    new SchemaError(new ::(new DuplicatedField(DynamicOptic(trace.toVector.reverse), fieldName), Nil))
+    new SchemaError(new ::(new DuplicatedField(toDynamicOptic(trace), fieldName), Nil))
 
   private[schema] def unknownCase(trace: List[DynamicOptic.Node], caseName: String): SchemaError =
-    new SchemaError(new ::(new UnknownCase(DynamicOptic(trace.toVector.reverse), caseName), Nil))
+    new SchemaError(new ::(new UnknownCase(toDynamicOptic(trace), caseName), Nil))
+
+  private[this] def toDynamicOptic(trace: List[DynamicOptic.Node]): DynamicOptic = {
+    val nodes = trace.toArray
+    if (nodes.length > 1) {
+      var idx1 = 0
+      var idx2 = nodes.length - 1
+      while (idx1 < idx2) {
+        val node = nodes(idx1)
+        nodes(idx1) = nodes(idx2)
+        nodes(idx2) = node
+        idx1 += 1
+        idx2 -= 1
+      }
+    }
+    new DynamicOptic(ArraySeq.unsafeWrapArray(nodes))
+  }
 
   sealed trait Single {
     def message: String
