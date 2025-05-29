2eaa2a066b59ee3a318ed6b0c1a2a5c9fa803334
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-05-29 17:52:14 +0200

    Remove `field` and `caseOf` macros

--- schema/shared/src/main/scala-3/zio/blocks/schema/CompanionOptics.scala
+++ schema/shared/src/main/scala-3/zio/blocks/schema/CompanionOptics.scala
@@ -26,12 +26,6 @@ trait CompanionOptics[S] {
 
   transparent inline def optic[A](inline path: S => A)(using schema: Schema[S]): Any =
     ${ CompanionOptics.optic('path, 'schema) }
-
-  inline def field[A](inline path: S => A)(using schema: Schema[S]): Lens[S, A] =
-    ${ CompanionOptics.field('path, 'schema) }
-
-  inline def caseOf[A <: S](using schema: Schema[S]): Prism[S, A] =
-    ${ CompanionOptics.caseOf('schema) }
 }
 
 private object CompanionOptics {
@@ -155,37 +149,5 @@ private object CompanionOptics {
     }
 
     toOptic(toPathBody(path.asTerm)).get
-  }
-
-  def field[S: Type, A: Type](path: Expr[S => A], schema: Expr[Schema[S]])(using q: Quotes): Expr[Lens[S, A]] = {
-    import q.reflect._
-
-    def fail(msg: String): Nothing = report.errorAndAbort(msg, Position.ofMacroExpansion)
-
-    def toPathBody(term: Term): Term = term match {
-      case Inlined(_, _, inlinedBlock) =>
-        toPathBody(inlinedBlock)
-      case Block(List(DefDef(_, _, _, Some(pathBody))), _) =>
-        pathBody
-      case _ =>
-        fail(s"Expected a lambda expression, got: ${term.show(using Printer.TreeStructure)}")
-    }
-
-    toPathBody(path.asTerm) match {
-      case Select(Ident(_), fieldName) =>
-        '{ $schema.reflect.asRecord.flatMap(_.lensByName[A](${ Expr(fieldName) })).get }.asExprOf[Lens[S, A]]
-      case term =>
-        fail(s"Expected a path element, got: ${term.show(using Printer.TreeStructure)}")
-    }
-  }
-
-  def caseOf[S: Type, A <: S: Type](schema: Expr[Schema[S]])(using q: Quotes): Expr[Prism[S, A]] = {
-    import q.reflect._
-
-    val aTpe     = TypeRepr.of[A]
-    var caseName = aTpe.typeSymbol.name
-    if (aTpe.termSymbol.flags.is(Flags.Enum)) caseName = aTpe.termSymbol.name
-    else if (aTpe.typeSymbol.flags.is(Flags.Module)) caseName = caseName.substring(0, caseName.length - 1)
-    '{ $schema.reflect.asVariant.flatMap(_.prismByName[A](${ Expr(caseName) })).get }.asExprOf[Prism[S, A]]
   }
 }
