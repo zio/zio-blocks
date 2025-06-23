42d36d6a601c3afbf79b960ae5ca7553ec8c3d5c
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-06-23 08:40:09 +0200

    Remove `Codec.format` to avoid cyclic dependencies between codecs and formats

--- schema/shared/src/main/scala/zio/blocks/schema/codec/Codec.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/codec/Codec.scala
@@ -2,16 +2,6 @@ package zio.blocks.schema.codec
 import zio.blocks.schema.SchemaError
 
 abstract class Codec[DecodeInput, EncodeOutput, Value] {
-
-  /**
-   * The format of the codec. For any given type `A`, there should be a unique
-   * `Format` that corresponds to the binary encoding of that type.
-   *
-   * The format tag is used to cache derivation of type classes on a per-schema
-   * basis.
-   */
-  def format: Format
-
   def encode(value: Value, output: EncodeOutput): Unit
 
   def decode(input: DecodeInput): Either[SchemaError, Value]
