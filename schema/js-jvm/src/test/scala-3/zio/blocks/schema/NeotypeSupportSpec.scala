cff68aa80cfb3ef6f2fcdc4e2d737519d6acb4e2
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-09-05 11:06:34 +0200

    Fix unexpected `TypeName` value when using neotype and zio-prelude newtypes inside an `Option`

--- schema/js-jvm/src/test/scala-3/zio/blocks/schema/NeotypeSupportSpec.scala
+++ schema/js-jvm/src/test/scala-3/zio/blocks/schema/NeotypeSupportSpec.scala
@@ -8,17 +8,17 @@ import zio.test._
 object NeotypeSupportSpec extends ZIOSpecDefault {
   def spec: Spec[TestEnvironment, Any] = suite("NeotypeSupportSpec")(
     test("derive schemas for cases classes with subtype and newtype fields") {
-      val value = new Planet(Name("Earth"), Kilogram(5.97e24), Meter(6378000.0))
+      val value = new Planet(Name("Earth"), Kilogram(5.97e24), Meter(6378000.0), Some(Meter(1.5e15)))
       assert(Planet.name.get(value))(equalTo(Name("Earth"))) &&
       assert(Planet.name_wrapped.getOption(value))(isSome(equalTo("Earth"))) &&
       assert(Planet.mass.get(value))(equalTo(Kilogram(5.97e24))) &&
       assert(Planet.radius.get(value))(equalTo(Meter(6378000.0))) &&
       assert(Planet.name_wrapped.replace(value, ""))(equalTo(value)) &&
       assert(Planet.name_wrapped.replace(value, "Cradle"))(
-        equalTo(new Planet(Name("Cradle"), Kilogram(5.97e24), Meter(6378000.0)))
+        equalTo(new Planet(Name("Cradle"), Kilogram(5.97e24), Meter(6378000.0), Some(Meter(1.5e15))))
       ) &&
       assert(Planet.mass.replace(value, Kilogram(5.970001e24)))(
-        equalTo(new Planet(Name("Earth"), Kilogram(5.970001e24), Meter(6378000.0)))
+        equalTo(new Planet(Name("Earth"), Kilogram(5.970001e24), Meter(6378000.0), Some(Meter(1.5e15))))
       ) &&
       assert(Planet.schema.fromDynamicValue(Planet.schema.toDynamicValue(value)))(isRight(equalTo(value))) &&
       assert(Planet.name.focus.typeName)(
@@ -34,6 +34,13 @@ object NeotypeSupportSpec extends ZIOSpecDefault {
       assert(Planet.radius.focus.typeName)(
         equalTo(
           TypeName[Meter](Namespace(Seq("zio", "blocks", "schema"), Seq("NeotypeSupportSpec")), "Meter")
+        )
+      ) &&
+      assert(Planet.distanceFromSun.focus.typeName)(
+        equalTo(
+          TypeName.option(
+            TypeName[Meter](Namespace(Seq("zio", "blocks", "schema"), Seq("NeotypeSupportSpec")), "Meter")
+          )
         )
       )
     }
@@ -55,13 +62,14 @@ object NeotypeSupportSpec extends ZIOSpecDefault {
 
   object Meter extends Newtype[Double]
 
-  case class Planet(name: Name, mass: Kilogram, radius: Meter)
+  case class Planet(name: Name, mass: Kilogram, radius: Meter, distanceFromSun: Option[Meter])
 
   object Planet extends CompanionOptics[Planet] {
-    implicit val schema: Schema[Planet]        = Schema.derived
-    val name: Lens[Planet, Name]               = $(_.name)
-    val mass: Lens[Planet, Kilogram]           = $(_.mass)
-    val radius: Lens[Planet, Meter]            = $(_.radius)
-    val name_wrapped: Optional[Planet, String] = $(_.name.wrapped[String])
+    implicit val schema: Schema[Planet]              = Schema.derived
+    val name: Lens[Planet, Name]                     = $(_.name)
+    val mass: Lens[Planet, Kilogram]                 = $(_.mass)
+    val radius: Lens[Planet, Meter]                  = $(_.radius)
+    val distanceFromSun: Lens[Planet, Option[Meter]] = $(_.distanceFromSun)
+    val name_wrapped: Optional[Planet, String]       = $(_.name.wrapped[String])
   }
 }
