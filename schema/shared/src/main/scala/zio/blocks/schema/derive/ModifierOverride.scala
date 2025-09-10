e62d327949da1617987bbe6356814fad7069743a
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-09-10 12:31:00 +0200

    Add an ability to override modifiers for terms (#340)

--- schema/shared/src/main/scala/zio/blocks/schema/derive/ModifierOverride.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/derive/ModifierOverride.scala
@@ -2,13 +2,27 @@ package zio.blocks.schema.derive
 
 import zio.blocks.schema._
 
-final case class ModifierOverride[A](overrideBy: ModifierOverride.By[A], modifier: Modifier)
+sealed trait ModifierOverride
 
-object ModifierOverride {
+case class ModifierReflectOverride[A](overrideBy: ModifierReflectOverride.By[A], modifier: Modifier.Reflect)
+    extends ModifierOverride
+
+object ModifierReflectOverride {
   sealed trait By[A]
 
   object By {
     case class Optic[A](optic: DynamicOptic) extends By[A]
     case class Type[A](name: TypeName[A])    extends By[A]
+  }
+}
+
+case class ModifierTermOverride[A](overrideBy: ModifierTermOverride.By[A], modifier: Modifier.Term)
+    extends ModifierOverride
+
+object ModifierTermOverride {
+  sealed trait By[A]
+
+  object By {
+    case class Type[A](name: TypeName[A], termName: String) extends By[A]
   }
 }
