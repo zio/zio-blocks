50df9891a7fba6b69dfac4195208b1573a18613c
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-10-02 10:26:00 +0200

    Add `TypeRegistry.default`

--- schema/shared/src/main/scala/zio/blocks/schema/registry/TypeRegistry.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/registry/TypeRegistry.scala
@@ -20,7 +20,43 @@ object TypeRegistry {
     def remove[A](typeName: TypeName[A]): Unit = map.remove(typeName)
   }
 
-  val empty: TypeRegistry = new Collection()
+  def empty: TypeRegistry = new Collection()
+
+  def default: TypeRegistry = {
+    val collection = new Collection()
+    collection.add(TypeName.unit, Binding.Primitive.unit)
+    collection.add(TypeName.boolean, Binding.Primitive.boolean)
+    collection.add(TypeName.byte, Binding.Primitive.byte)
+    collection.add(TypeName.short, Binding.Primitive.short)
+    collection.add(TypeName.int, Binding.Primitive.int)
+    collection.add(TypeName.long, Binding.Primitive.long)
+    collection.add(TypeName.float, Binding.Primitive.float)
+    collection.add(TypeName.double, Binding.Primitive.double)
+    collection.add(TypeName.char, Binding.Primitive.char)
+    collection.add(TypeName.string, Binding.Primitive.string)
+    collection.add(TypeName.bigInt, Binding.Primitive.bigInt)
+    collection.add(TypeName.bigDecimal, Binding.Primitive.bigDecimal)
+    collection.add(TypeName.dayOfWeek, Binding.Primitive.dayOfWeek)
+    collection.add(TypeName.duration, Binding.Primitive.duration)
+    collection.add(TypeName.instant, Binding.Primitive.instant)
+    collection.add(TypeName.localDate, Binding.Primitive.localDate)
+    collection.add(TypeName.localDateTime, Binding.Primitive.localDateTime)
+    collection.add(TypeName.localTime, Binding.Primitive.localTime)
+    collection.add(TypeName.month, Binding.Primitive.month)
+    collection.add(TypeName.monthDay, Binding.Primitive.monthDay)
+    collection.add(TypeName.offsetDateTime, Binding.Primitive.offsetDateTime)
+    collection.add(TypeName.offsetTime, Binding.Primitive.offsetTime)
+    collection.add(TypeName.period, Binding.Primitive.period)
+    collection.add(TypeName.year, Binding.Primitive.year)
+    collection.add(TypeName.yearMonth, Binding.Primitive.yearMonth)
+    collection.add(TypeName.zoneId, Binding.Primitive.zoneId)
+    collection.add(TypeName.zoneOffset, Binding.Primitive.zoneOffset)
+    collection.add(TypeName.zonedDateTime, Binding.Primitive.zonedDateTime)
+    collection.add(TypeName.currency, Binding.Primitive.currency)
+    collection.add(TypeName.uuid, Binding.Primitive.uuid)
+    collection.add(TypeName.uuid, Binding.Primitive.uuid)
+    collection
+  }
 }
 
 case class RebindError(message: String)
