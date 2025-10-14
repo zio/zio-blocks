62d6110111e1741c165ae0cdf19fa64ffc11c5ca
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-10-14 12:14:29 +0200

    Clean up of benchmarks

--- benchmarks/src/main/scala/zio/blocks/avro/ListOfRecordsBenchmark.scala
+++ benchmarks/src/main/scala/zio/blocks/avro/ListOfRecordsBenchmark.scala
@@ -7,18 +7,17 @@ import zio.blocks.schema.Schema
 import zio.blocks.avro.AvroFormat
 import zio.schema.codec.AvroCodec
 import zio.schema.{DeriveSchema, Schema => ZIOSchema}
+import java.nio.ByteBuffer
 
 class ListOfRecordsBenchmark extends BaseBenchmark {
   import ListOfRecordsDomain._
 
   @Benchmark
-  def readingZioBlocks: List[Person] = {
-    val byteBuffer = java.nio.ByteBuffer.wrap(encodedListOfRecords)
-    zioBlocksSchema.decode(AvroFormat)(byteBuffer) match {
+  def readingZioBlocks: List[Person] =
+    zioBlocksSchema.decode(AvroFormat)(ByteBuffer.wrap(encodedListOfRecords)) match {
       case Right(value) => value
       case Left(error)  => sys.error(error.getMessage)
     }
-  }
 
   @Benchmark
   def readingZioSchema: List[Person] =
@@ -29,7 +28,7 @@ class ListOfRecordsBenchmark extends BaseBenchmark {
 
   @Benchmark
   def writingZioBlocks: Array[Byte] = {
-    val byteBuffer = java.nio.ByteBuffer.allocate(1024)
+    val byteBuffer = ByteBuffer.allocate(1024)
     zioBlocksSchema.encode(AvroFormat)(byteBuffer)(listOfRecords)
     java.util.Arrays.copyOf(byteBuffer.array, byteBuffer.position)
   }
@@ -41,28 +40,14 @@ class ListOfRecordsBenchmark extends BaseBenchmark {
 object ListOfRecordsDomain {
   case class Person(id: Long, name: String, age: Int, address: String, childrenAges: List[Int])
 
-  object Person {
-    implicit val zioSchema: ZIOSchema[Person] = DeriveSchema.gen[Person]
-  }
-
-  private val person = Person(12345678901L, "John", 30, "123 Main St", List(5, 7, 9))
+  implicit val zioSchema: ZIOSchema[Person] = DeriveSchema.gen[Person]
 
   implicit val zioBlocksSchema: Schema[List[Person]] = Schema.derived
 
   val zioSchemaCodec: AvroCodec.ExtendedBinaryCodec[List[Person]] = AvroCodec.schemaBasedBinaryCodec[List[Person]]
 
-  val listOfRecords: List[Person] = List(
-    person,
-    person,
-    person,
-    person,
-    person,
-    person,
-    person,
-    person,
-    person,
-    person
-  )
+  val listOfRecords: List[Person] =
+    (1 to 10).map(_ => Person(12345678901L, "John", 30, "123 Main St", List(5, 7, 9))).toList
 
   val encodedListOfRecords: Array[Byte] = zioSchemaCodec.encode(listOfRecords).toArray
 }
