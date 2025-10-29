6e21841b42d190d41ce6cd0418623bc48a7fc788
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-10-29 09:32:12 +0100

    Reuse schema instances for options

--- schema/shared/src/test/scala-3/zio/blocks/schema/OpaqueTypes.scala
+++ schema/shared/src/test/scala-3/zio/blocks/schema/OpaqueTypes.scala
@@ -5,3 +5,7 @@ opaque type Variant = Int | String | Boolean
 object Variant {
   def apply(v: Int | String | Boolean): Variant = v
 }
+
+opaque type StructureId <: String = String
+
+given Schema[StructureId] = Schema.string
