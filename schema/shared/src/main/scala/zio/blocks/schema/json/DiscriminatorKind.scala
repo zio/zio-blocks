444673452b3fb0a0eb9ad9d35df6b1a63c0d0fd6
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-12-02 14:58:58 +0100

    Add an option for `JsonBinaryCodecDeriver` to support discriminator represented by JSON key-value pair for ADT where all cases are records

---
+++ schema/shared/src/main/scala/zio/blocks/schema/json/DiscriminatorKind.scala
@@ -0,0 +1,9 @@
+package zio.blocks.schema.json
+
+sealed trait DiscriminatorKind
+
+object DiscriminatorKind {
+  case class Field(name: String) extends DiscriminatorKind
+
+  case object Key extends DiscriminatorKind
+}
