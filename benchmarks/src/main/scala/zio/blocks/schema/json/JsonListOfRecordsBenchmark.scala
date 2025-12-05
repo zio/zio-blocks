993d8314f3a371bbeb99f0607b6650aee2b062a7
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-12-05 16:52:03 +0100

    Add JSON benchmarks for zio-schema

--- benchmarks/src/main/scala/zio/blocks/schema/json/JsonListOfRecordsBenchmark.scala
+++ benchmarks/src/main/scala/zio/blocks/schema/json/JsonListOfRecordsBenchmark.scala
@@ -6,6 +6,7 @@ import org.openjdk.jmh.annotations._
 import zio.blocks.BaseBenchmark
 import zio.blocks.schema.Schema
 import zio.schema.{DeriveSchema, Schema as ZIOSchema}
+import java.nio.charset.StandardCharsets.UTF_8
 
 class JsonListOfRecordsBenchmark extends BaseBenchmark {
   import JsonListOfRecordsDomain._
@@ -33,9 +34,17 @@ class JsonListOfRecordsBenchmark extends BaseBenchmark {
   @Benchmark
   def readingZioJson: List[Person] = {
     import zio.json.DecoderOps
-    import java.nio.charset.StandardCharsets.UTF_8
 
-    new String(encodedListOfRecords, UTF_8).fromJson[List[Person]].fold(sys.error, identity)
+    new String(encodedListOfRecords, UTF_8).fromJson[List[Person]] match {
+      case Right(value) => value
+      case Left(error)  => sys.error(error)
+    }
+  }
+
+  @Benchmark
+  def readingZioSchema: List[Person] = zioSchemaCodec.decodeJson(new String(encodedListOfRecords, UTF_8)) match {
+    case Right(value) => value
+    case Left(error)  => sys.error(error)
   }
 
   @Benchmark
@@ -47,20 +56,23 @@ class JsonListOfRecordsBenchmark extends BaseBenchmark {
   @Benchmark
   def writingZioJson: Array[Byte] = {
     import zio.json.EncoderOps
-    import java.nio.charset.StandardCharsets.UTF_8
 
     listOfRecords.toJson.getBytes(UTF_8)
   }
+
+  @Benchmark
+  def writingZioSchema: Array[Byte] = zioSchemaCodec.encodeJson(listOfRecords, None).toString.getBytes(UTF_8)
 }
 
 object JsonListOfRecordsDomain {
   case class Person(id: Long, name: String, age: Int, address: String, childrenAges: List[Int])
 
-  implicit val zioSchema: ZIOSchema[Person] = DeriveSchema.gen[Person]
-
   implicit val jsoniterScalaCodec: JsonValueCodec[List[Person]] = JsonCodecMaker.make
+
+  val zioBlocksCodec: JsonBinaryCodec[List[Person]] = Schema.derived.derive(JsonFormat.deriver)
 
   implicit val zioJsonCodec: zio.json.JsonCodec[Person] = zio.json.DeriveJsonCodec.gen
 
-  val zioBlocksCodec: JsonBinaryCodec[List[Person]] = Schema.derived.deriving(JsonFormat.deriver).derive
+  val zioSchemaCodec: zio.json.JsonCodec[List[Person]] =
+    zio.schema.codec.JsonCodec.jsonCodec(DeriveSchema.gen[List[Person]])
 }
