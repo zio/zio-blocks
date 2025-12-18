935c2f72a115670c3f70b889563f308cf44aca36
codingsh <codingsh@pm.me>
2025-12-18 20:22:07 +0000

    feat(macro): implement ToDynamicOptic macro for selector parsing

--- schema/shared/src/main/scala/zio/blocks/schema/migration/Macro.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/migration/Macro.scala
@@ -1,40 +1,64 @@
-/*
 package zio.blocks.schema.migration
 
 import zio.blocks.schema.DynamicOptic
+
 import scala.quoted._
 
-object Macro {
-  trait PathSelector[S] {
-    def apply(): DynamicOptic
-  }
+// A type class to convert a selector function S => A into a DynamicOptic
+trait ToDynamicOptic[S, A] {
+  def apply(): DynamicOptic
+}
 
-  def path[S, A](f: S => A): PathSelector[S] = new PathSelector[S] {
-    def apply(): DynamicOptic = DynamicOptic.root
-  }
+object ToDynamicOptic {
+  implicit inline def materialize[S, A]: ToDynamicOptic[S, A] =
+    ${ ToDynamicOpticMacro.materialize[S, A] }
+}
 
-  inline def toPath[S, A](inline f: S => A): DynamicOptic = ${ toPathImpl('f) }
-
-  def toPathImpl[S: Type, A: Type](f: Expr[S => A])(using Quotes): Expr[DynamicOptic] = {
+object ToDynamicOpticMacro {
+  def materialize[S: Type, A: Type](using Quotes): Expr[ToDynamicOptic[S, A]] = {
     import quotes.reflect._
 
-    def extractPath(term: Term): DynamicOptic = term match {
-      case Select(rest, name) => extractPath(rest).field(name)
-      case Apply(Select(rest, "apply"), List(Literal(IntConstant(i)))) => extractPath(rest).at(i)
-      case Ident(_) => DynamicOptic.root
-      case _ => report.errorAndAbort(s"Unsupported path element: $term")
+    def extractPath(term: Term): DynamicOptic = {
+      term match {
+        case Select(qualifier, name) =>
+          val parentOptic = extractPath(qualifier)
+          name match {
+            case "each"      => parentOptic.elements
+            case "eachKey"   => parentOptic.mapKeys
+            case "eachValue" => parentOptic.mapValues
+            case _           => parentOptic.field(name)
+          }
+        case Apply(Select(qualifier, "apply"), List(Literal(IntConstant(index)))) =>
+          val parentOptic = extractPath(qualifier)
+          parentOptic.at(index)
+        case Apply(Select(qualifier, "at"), List(Literal(IntConstant(index)))) =>
+          val parentOptic = extractPath(qualifier)
+          parentOptic.at(index)
+        case Apply(Select(qualifier, "field"), List(Literal(StringConstant(name)))) =>
+          val parentOptic = extractPath(qualifier)
+          parentOptic.field(name)
+        case TypeApply(Select(qualifier, "when"), List(AppliedType(_, List(Ident(caseName))))) =>
+          val parentOptic = extractPath(qualifier)
+          parentOptic.caseOf(caseName)
+        case Ident(name) if name == "x" => // Assuming 'x' is the parameter name for the lambda `s => ...`
+          DynamicOptic.root
+        case _ =>
+          report.errorAndAbort(s"Unsupported selector expression: ${term.show}")
+      }
     }
 
-    val optic = f.asTerm match {
-      case Inlined(_, _, Block(List(DefDef(_, _, _, Some(term))), _)) => extractPath(term)
-      case _ => report.errorAndAbort(s"Unsupported function shape: ${f.asTerm}")
-    }
+    val lambdaTree = Expr.betaReduce('{ (s: S) => ??? }).asTerm // Dummy lambda to get the type of S
 
-    '{
-      new PathSelector[S] {
-        def apply(): DynamicOptic = ${ Expr(optic) }
-      }.apply()
+    lambdaTree match {
+      case Lambda(List(param), body) =>
+        val optic = extractPath(body)
+        '{
+          new ToDynamicOptic[S, A] {
+            def apply(): DynamicOptic = ${ Expr(optic) }
+          }
+        }
+      case _ =>
+        report.errorAndAbort("Expected a lambda expression for the selector.")
     }
   }
 }
-*/
