52242661ff7e54ffc754a3c9aad43b1b4f99c49f
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2025-12-19 14:48:17 +0100

    Flatten variant hierarchy during derivation of JSON codecs (#530)

--- schema/shared/src/test/scala-3/zio/blocks/schema/json/JsonBinaryCodecDeriverVersionSpecificSpec.scala
+++ schema/shared/src/test/scala-3/zio/blocks/schema/json/JsonBinaryCodecDeriverVersionSpecificSpec.scala
@@ -33,8 +33,8 @@ object JsonBinaryCodecDeriverVersionSpecificSpec extends ZIOSpecDefault {
     ),
     suite("variants")(
       test("constant values on different hierarchy levels") {
-        roundTrip[Foo](Foo1, """{"Foo1":{}}""") &&
-        roundTrip[Foo](Bar1, """{"Bar":"Bar1"}""")
+        roundTrip[Foo](Foo1, """"Foo1"""") &&
+        roundTrip[Foo](Bar1, """"Bar1"""")
       },
       test("constant values") {
         roundTrip[TrafficLight](TrafficLight.Green, """"Green"""") &&
