c784145036fcb52cd88955634ec59ca46be0bbf8
natinew77-creator <nati.new77@gmail.com>
2025-12-25 22:23:52 -0500

    fix: Move wrapper methods to Schema object and refine OpticsHolder hierarchy

--- schema/shared/src/main/scala-3/zio/blocks/schema/DerivedOptics.scala
+++ schema/shared/src/main/scala-3/zio/blocks/schema/DerivedOptics.scala
@@ -63,7 +63,7 @@ trait DerivedOptics[S] {
  * An optics holder that stores lenses/prisms in a map and provides dynamic
  * access. This is an implementation detail and should not be used directly.
  */
-final class OpticsHolder(members: Map[String, Any]) extends scala.Dynamic with Selectable {
+final class OpticsHolder(members: Map[String, Any]) extends Selectable {
   def selectDynamic(name: String): Any =
     members.getOrElse(name, throw new RuntimeException(s"No optic found for: $name"))
 }
