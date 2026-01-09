c38dd96958264cd51816fc9bc48d56cc3c800ade
Ajay RV <ajayrv6711@gmail.com>
2026-01-09 16:19:47 +0530

    housekeeping

--- schema/shared/src/main/scala/zio/blocks/schema/patch/PatchSchemas.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/patch/PatchSchemas.scala
@@ -2,11 +2,6 @@ package zio.blocks.schema.patch
 
 import zio.blocks.schema.{DynamicPatch, PatchMode, Schema}
 
-/**
- * Schema instances for patch types. These are defined in test code to avoid
- * Scala 2 macro issues where Schema.derived cannot be used in the same
- * compilation unit as the Schema definition.
- */
 object PatchSchemas {
 
   implicit val stringOpSchema: Schema[StringOp] = Schema.derived
