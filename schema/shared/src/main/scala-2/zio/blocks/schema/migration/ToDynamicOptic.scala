d5e58db51ae5081ce078a87fee449a81ef6beab0
Mehmed Hasan Rajim <242034027@student.green.ac.bd>
2026-01-15 11:54:21 +0600

    fix: correct scala 2 macro syntax in ToDynamicOptic

--- schema/shared/src/main/scala-2/zio/blocks/schema/migration/ToDynamicOptic.scala
+++ schema/shared/src/main/scala-2/zio/blocks/schema/migration/ToDynamicOptic.scala
@@ -1,15 +1,14 @@
 package zio.blocks.schema.migration
 
 import zio.blocks.schema.DynamicOptic
+import scala.language.experimental.macros
+import scala.language.implicitConversions
 
 trait ToDynamicOptic[S, A] {
   def apply(): DynamicOptic
 }
 
 object ToDynamicOptic {
-  import scala.language.experimental.macros
-  import scala.language.implicitConversions
-
   implicit def derive[S, A](selector: S => A): ToDynamicOptic[S, A] = macro
     zio.blocks.schema.migration.macros.AccessorMacros.deriveImpl[S, A]
 }
