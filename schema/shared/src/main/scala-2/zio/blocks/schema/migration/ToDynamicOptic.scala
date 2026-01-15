a1c346882d612939cf1cda9d4709e63612706923
Mehmed Hasan Rajim <242034027@student.green.ac.bd>
2026-01-15 18:45:26 +0600

    fix: resolve scala 2 macro syntax and clean up imports

--- schema/shared/src/main/scala-2/zio/blocks/schema/migration/ToDynamicOptic.scala
+++ schema/shared/src/main/scala-2/zio/blocks/schema/migration/ToDynamicOptic.scala
@@ -1,14 +1,20 @@
 package zio.blocks.schema.migration
 
 import zio.blocks.schema.DynamicOptic
-import scala.language.experimental.macros
-import scala.language.implicitConversions
+import zio.blocks.schema.migration.macros.AccessorMacros
 
 trait ToDynamicOptic[S, A] {
   def apply(): DynamicOptic
 }
 
+/**
+ * Note: The @nowarn("msg=unused import") was removed because the compiler
+ * detected that all imports inside this object are actually used, making the
+ * suppression annotation redundant.
+ */
 object ToDynamicOptic {
-  implicit def derive[S, A](selector: S => A): ToDynamicOptic[S, A] = macro
-    zio.blocks.schema.migration.macros.AccessorMacros.deriveImpl[S, A]
+  import scala.language.experimental.macros
+  import scala.language.implicitConversions
+
+  implicit def derive[S, A](selector: S => A): ToDynamicOptic[S, A] = macro AccessorMacros.deriveImpl[S, A]
 }
