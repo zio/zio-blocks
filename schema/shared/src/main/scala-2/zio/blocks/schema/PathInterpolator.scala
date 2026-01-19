4628acfb3811b0fa21c329c1e18d0b3f19f31e75
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2026-01-19 20:55:30 +0100

    Format Scala 2 code with scalafmt

--- schema/shared/src/main/scala-2/zio/blocks/schema/PathInterpolator.scala
+++ schema/shared/src/main/scala-2/zio/blocks/schema/PathInterpolator.scala
@@ -3,7 +3,7 @@ package zio.blocks.schema
 import scala.language.experimental.macros
 
 trait PathInterpolator {
-  
+
   implicit class PathInterpolatorOps(val sc: StringContext) {
     def p(args: Any*): DynamicOptic = macro PathMacros.pImpl
   }
