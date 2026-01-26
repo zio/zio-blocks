16109c6e7ba10edd9a7ef17d7dbebf4e149ee603
Ajay RV <ajayrv6711@gmail.com>
2026-01-27 03:36:19 +0530

    Enchanced Json Interpolator (#852)
    * json interpolator implementation
    
    * more tests
    
    * fmt and improvements
    
    * thrift tests
    
    * coverage
    
    * more fixes
    
    * house cleaning
    
    * rebase fixes
    
    * review fixes
    
    * scala 2 fixes

---
+++ schema/shared/src/main/scala/zio/blocks/schema/json/InterpolationContext.scala
@@ -0,0 +1,37 @@
+package zio.blocks.schema.json
+
+/**
+ * Represents the context in which a JSON interpolation occurs.
+ *
+ * Used by the JSON string interpolator macro to determine type constraints:
+ *   - Key: Requires `Keyable[A]` - only types that can be used as JSON keys
+ *   - Value: Requires `JsonEncoder[A]` - any type that can be encoded to JSON
+ *   - InString: Requires `Keyable[A]` - embedded within a JSON string literal
+ */
+private[schema] sealed trait InterpolationContext
+
+private[schema] object InterpolationContext {
+
+  /**
+   * Interpolation in key position: `{$key: ...}`
+   *
+   * Only "keyable" types (those defined in `PrimitiveType`) are allowed. The
+   * value will be converted to a string using `Keyable[A].asKey`.
+   */
+  case object Key extends InterpolationContext
+
+  /**
+   * Interpolation in value position: `{...: $value}` or `[$value, ...]`
+   *
+   * Any type with a `JsonEncoder[A]` instance is allowed.
+   */
+  case object Value extends InterpolationContext
+
+  /**
+   * Interpolation inside a string literal: `{...: "hello $name"}`
+   *
+   * Only "keyable" types are allowed. The value will be converted to a string
+   * using `Keyable[A].asKey` and concatenated into the JSON string.
+   */
+  case object InString extends InterpolationContext
+}
