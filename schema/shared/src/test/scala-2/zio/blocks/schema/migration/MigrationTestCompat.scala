589504eda097e509c32fcf4f7a1f0e03d4c52c2c
Ajay RV <ajayrv6711@gmail.com>
2026-01-26 19:15:44 +0530

    scala2 typelevel

--- schema/shared/src/test/scala-2/zio/blocks/schema/migration/MigrationTestCompat.scala
+++ schema/shared/src/test/scala-2/zio/blocks/schema/migration/MigrationTestCompat.scala
@@ -5,7 +5,9 @@ import scala.language.implicitConversions
 // Scala 2: Re-export the implicit conversion so it's available via import for test
 object MigrationTestCompat {
   // Re-export the implicit conversion from MigrationBuilderSyntax
-  implicit def toSyntax[A, B](builder: MigrationBuilder[A, B]): MigrationBuilderSyntax[A, B] =
+  implicit def toSyntax[A, B, Handled, Provided](
+    builder: MigrationBuilder[A, B, Handled, Provided]
+  ): MigrationBuilderSyntax[A, B, Handled, Provided] =
     MigrationBuilderSyntax.toSyntax(builder)
 
   // Dummy method to make import usage consistent across Scala 2 and 3
