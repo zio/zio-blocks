2999bab8988c87baeced25d3bc18829616bb75bc
Ajay RV <ajayrv6711@gmail.com>
2026-01-29 09:52:03 +0530

    scala 3 extract full path

--- schema/shared/src/main/scala-3/zio/blocks/schema/migration/ValidationProof.scala
+++ schema/shared/src/main/scala-3/zio/blocks/schema/migration/ValidationProof.scala
@@ -8,17 +8,17 @@ import zio.blocks.schema.migration.FieldExtraction._
  * Compile-time proof that a migration is complete.
  *
  * A migration from A to B is complete when:
- *   - All "removed" fields (in A but not in B) are handled
- *   - All "added" fields (in B but not in A) are provided
+ *   - All "removed" paths (in A but not in B) are handled
+ *   - All "added" paths (in B but not in A) are provided
  *
- * Fields that exist in both A and B (unchanged fields) are automatically
- * handled/provided.
+ * Paths that exist in both A and B (unchanged paths) are automatically
+ * handled/provided. This includes nested paths like "address.street".
  *
  * Type parameters:
  *   - A: Source type
  *   - B: Target type
- *   - Handled: Tuple of field names that have been handled (from source)
- *   - Provided: Tuple of field names that have been provided (for target)
+ *   - Handled: Tuple of field paths that have been handled (from source)
+ *   - Provided: Tuple of field paths that have been provided (for target)
  */
 sealed trait ValidationProof[A, B, Handled <: Tuple, Provided <: Tuple]
 
@@ -31,16 +31,16 @@ object ValidationProof {
   final class Impl[A, B, Handled <: Tuple, Provided <: Tuple] extends ValidationProof[A, B, Handled, Provided]
 
   /**
-   * Type alias for required fields to handle (fields in A that are not in B).
+   * Type alias for required paths to handle (paths in A that are not in B).
    */
-  type RequiredHandled[A, B, FieldsA <: Tuple, FieldsB <: Tuple] =
-    Difference[FieldsA, FieldsB]
+  type RequiredHandled[A, B, PathsA <: Tuple, PathsB <: Tuple] =
+    Difference[PathsA, PathsB]
 
   /**
-   * Type alias for required fields to provide (fields in B that are not in A).
+   * Type alias for required paths to provide (paths in B that are not in A).
    */
-  type RequiredProvided[A, B, FieldsA <: Tuple, FieldsB <: Tuple] =
-    Difference[FieldsB, FieldsA]
+  type RequiredProvided[A, B, PathsA <: Tuple, PathsB <: Tuple] =
+    Difference[PathsB, PathsA]
 
   /**
    * Derive a ValidationProof when the migration is complete.
@@ -48,13 +48,16 @@ object ValidationProof {
    * This given instance only exists when:
    *   - IsSubset[RequiredHandled, Handled] =:= true
    *   - IsSubset[RequiredProvided, Provided] =:= true
+   *
+   * Uses FieldPaths to extract full nested paths (e.g., "address.street")
+   * rather than just top-level field names.
    */
   transparent inline given derive[A, B, Handled <: Tuple, Provided <: Tuple](using
-    fnA: FieldNames[A],
-    fnB: FieldNames[B]
+    fpA: FieldPaths[A],
+    fpB: FieldPaths[B]
   ): ValidationProof[A, B, Handled, Provided] =
-    summonFrom { case _: (IsSubset[Difference[fnA.Labels, fnB.Labels], Handled] =:= true) =>
-      summonFrom { case _: (IsSubset[Difference[fnB.Labels, fnA.Labels], Provided] =:= true) =>
+    summonFrom { case _: (IsSubset[Difference[fpA.Paths, fpB.Paths], Handled] =:= true) =>
+      summonFrom { case _: (IsSubset[Difference[fpB.Paths, fpA.Paths], Provided] =:= true) =>
         new Impl[A, B, Handled, Provided]
       }
     }
@@ -62,8 +65,8 @@ object ValidationProof {
   /**
    * Helper type to compute whether a migration is valid.
    */
-  type IsValid[A, B, Handled <: Tuple, Provided <: Tuple, FieldsA <: Tuple, FieldsB <: Tuple] =
-    (IsSubset[Difference[FieldsA, FieldsB], Handled], IsSubset[Difference[FieldsB, FieldsA], Provided]) match {
+  type IsValid[A, B, Handled <: Tuple, Provided <: Tuple, PathsA <: Tuple, PathsB <: Tuple] =
+    (IsSubset[Difference[PathsA, PathsB], Handled], IsSubset[Difference[PathsB, PathsA], Provided]) match {
       case (true, true) => true
       case _            => false
     }
@@ -75,11 +78,11 @@ object ValidationProof {
    * compile error messages when validation fails.
    */
   inline def require[A, B, Handled <: Tuple, Provided <: Tuple](using
-    fnA: FieldNames[A],
-    fnB: FieldNames[B]
+    fpA: FieldPaths[A],
+    fpB: FieldPaths[B]
   ): ValidationProof[A, B, Handled, Provided] = {
-    summonInline[IsSubset[Difference[fnA.Labels, fnB.Labels], Handled] =:= true]
-    summonInline[IsSubset[Difference[fnB.Labels, fnA.Labels], Provided] =:= true]
+    summonInline[IsSubset[Difference[fpA.Paths, fpB.Paths], Handled] =:= true]
+    summonInline[IsSubset[Difference[fpB.Paths, fpA.Paths], Provided] =:= true]
     new Impl[A, B, Handled, Provided]
   }
 }
