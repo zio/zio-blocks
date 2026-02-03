4beb78024e53a14f6a3029931943c9f719ab1d3b
Mehmed Hasan Rajim <242034027@student.green.ac.bd>
2026-02-03 12:09:01 +0600

    fix: schema migration engine for issue #519

--- New folder/shared/src/main/scala-3/zio/blocks/schema/PathInterpolator.scala
+++
@@ -1,8 +0,0 @@
-package zio.blocks.schema
-
-trait PathInterpolator {
-
-  extension (inline sc: StringContext) {
-    transparent inline def p(inline args: Any*): DynamicOptic = ${ PathMacros.pImpl('sc, 'args) }
-  }
-}
--- New folder/shared/src/main/scala/zio/blocks/schema/derive/HasInstance.scala
+++
@@ -1,7 +0,0 @@
-package zio.blocks.schema.derive
-
-import zio.blocks.schema.Lazy
-
-trait HasInstance[F[_, _], TC[_]] {
-  def instance[T, A](fa: F[T, A]): Lazy[TC[A]]
-}
