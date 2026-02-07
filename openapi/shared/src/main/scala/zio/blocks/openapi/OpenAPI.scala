51545e03f6e15c72c60ad289064a93df6e0ae849
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2026-02-07 21:00:49 +0100

    feat(openapi): add SchemaObject, Operation, Parameter, Header types
    - Task 7: SchemaObject wrapping JsonSchema with OpenAPI vocabulary
    - Task 10: Operation with required responses field
    - Task 11: Parameter, Header, ParameterLocation with path validation
    
    148 tests passing, all types have Schema derivation

--- openapi/shared/src/main/scala/zio/blocks/openapi/OpenAPI.scala
+++ openapi/shared/src/main/scala/zio/blocks/openapi/OpenAPI.scala
@@ -299,6 +299,57 @@ object ExternalDocumentation {
 }
 
 /**
+ * Describes a single API operation on a path.
+ *
+ * @param responses
+ *   REQUIRED. The list of possible responses as they are returned from
+ *   executing this operation.
+ * @param tags
+ *   A list of tags for API documentation control.
+ * @param summary
+ *   A short summary of what the operation does.
+ * @param description
+ *   A verbose explanation of the operation behavior.
+ * @param externalDocs
+ *   Additional external documentation for this operation.
+ * @param operationId
+ *   Unique string used to identify the operation.
+ * @param parameters
+ *   A list of parameters that are applicable for this operation.
+ * @param requestBody
+ *   The request body applicable for this operation.
+ * @param callbacks
+ *   A map of possible out-of band callbacks related to the parent operation.
+ * @param deprecated
+ *   Declares this operation to be deprecated.
+ * @param security
+ *   A declaration of which security mechanisms can be used for this operation.
+ * @param servers
+ *   An alternative server array to service this operation.
+ * @param extensions
+ *   Extension fields starting with x-.
+ */
+final case class Operation(
+  responses: Json,
+  tags: List[String] = Nil,
+  summary: Option[String] = None,
+  description: Option[String] = None,
+  externalDocs: Option[ExternalDocumentation] = None,
+  operationId: Option[String] = None,
+  parameters: List[Json] = Nil,
+  requestBody: Option[Json] = None,
+  callbacks: Map[String, Json] = Map.empty,
+  deprecated: Boolean = false,
+  security: List[SecurityRequirement] = Nil,
+  servers: List[Server] = Nil,
+  extensions: Map[String, Json] = Map.empty
+)
+
+object Operation {
+  implicit val schema: Schema[Operation] = Schema.derived
+}
+
+/**
  * A simple object to allow referencing other components in the OpenAPI
  * document, internally and externally.
  *
@@ -349,4 +400,327 @@ object ReferenceOr {
   final case class Value[A](value: A) extends ReferenceOr[A]
 
   implicit def schema[A: Schema]: Schema[ReferenceOr[A]] = Schema.derived
+}
+
+/**
+ * When request bodies or response payloads may be one of a number of different
+ * schemas, a discriminator object can be used to aid in serialization,
+ * deserialization, and validation.
+ *
+ * The discriminator is a specific object in a schema which is used to inform
+ * the consumer of the document of an alternative schema based on the value
+ * associated with it.
+ *
+ * @param propertyName
+ *   REQUIRED. The name of the property in the payload that will hold the
+ *   discriminator value.
+ * @param mapping
+ *   An object to hold mappings between payload values and schema names or
+ *   references.
+ */
+final case class Discriminator(
+  propertyName: String,
+  mapping: Map[String, String] = Map.empty
+)
+
+object Discriminator {
+  implicit val schema: Schema[Discriminator] = Schema.derived
+}
+
+/**
+ * A metadata object that allows for more fine-tuned XML model definitions.
+ *
+ * When using arrays, XML element names are not inferred (for singular/plural
+ * forms) and the name property SHOULD be used to add that information.
+ *
+ * @param name
+ *   Replaces the name of the element/attribute used for the described schema
+ *   property. When defined within items, it will affect the name of the
+ *   individual XML elements within the list. When defined alongside type being
+ *   array (outside the items), it will affect the wrapping element and only if
+ *   wrapped is true. If wrapped is false, it will be ignored.
+ * @param namespace
+ *   The URI of the namespace definition. This MUST be in the form of an
+ *   absolute URI.
+ * @param prefix
+ *   The prefix to be used for the name.
+ * @param attribute
+ *   Declares whether the property definition translates to an attribute instead
+ *   of an element. Default value is false.
+ * @param wrapped
+ *   MAY be used only for an array definition. Signifies whether the array is
+ *   wrapped (for example, `<books><book/><book/></books>`) or unwrapped
+ *   (`<book/><book/>`). Default value is false. The definition takes effect
+ *   only when defined alongside type being array (outside the items).
+ */
+final case class XML(
+  name: Option[String] = None,
+  namespace: Option[String] = None,
+  prefix: Option[String] = None,
+  attribute: Boolean = false,
+  wrapped: Boolean = false
+)
+
+object XML {
+  implicit val schema: Schema[XML] = Schema.derived
+}
+
+/**
+ * The Schema Object allows the definition of input and output data types. These
+ * types can be objects, but also primitives and arrays.
+ *
+ * In OpenAPI 3.1, the Schema Object is fully compatible with JSON Schema
+ * 2020-12, with the addition of OpenAPI-specific vocabulary.
+ *
+ * This implementation wraps the existing JsonSchema from zio-blocks-schema and
+ * adds OpenAPI-specific vocabulary fields.
+ *
+ * @param jsonSchema
+ *   The underlying JSON Schema 2020-12 schema represented as Json. This
+ *   contains all standard JSON Schema keywords (type, properties, items, etc.).
+ * @param discriminator
+ *   Adds support for polymorphism. The discriminator is an object name that is
+ *   used to differentiate between other schemas which may satisfy the payload
+ *   description.
+ * @param xml
+ *   This MAY be used only on properties schemas. It has no effect on root
+ *   schemas. Adds additional metadata to describe the XML representation of
+ *   this property.
+ * @param externalDocs
+ *   Additional external documentation for this schema.
+ * @param example
+ *   A free-form property to include an example of an instance for this schema.
+ *   To represent examples that cannot be naturally represented in JSON or YAML,
+ *   a string value can be used to contain the example with escaping where
+ *   necessary. Deprecated: The example property has been deprecated in favor of
+ *   the JSON Schema examples keyword. Use of example is discouraged, and later
+ *   versions of this specification may remove it.
+ * @param extensions
+ *   Specification extensions (x-* fields). These allow adding additional
+ *   properties beyond the standard OpenAPI fields.
+ */
+final case class SchemaObject(
+  jsonSchema: Json,
+  discriminator: Option[Discriminator] = None,
+  xml: Option[XML] = None,
+  externalDocs: Option[ExternalDocumentation] = None,
+  example: Option[Json] = None,
+  extensions: Map[String, Json] = Map.empty
+) {
+
+  /**
+   * Extracts the underlying JSON Schema as Json.
+   */
+  def toJson: Json = jsonSchema
+
+  /**
+   * Attempts to parse the underlying JSON Schema.
+   */
+  def toJsonSchema: Either[zio.blocks.schema.SchemaError, zio.blocks.schema.json.JsonSchema] =
+    zio.blocks.schema.json.JsonSchema.fromJson(jsonSchema)
+}
+
+object SchemaObject {
+
+  /**
+   * Creates a SchemaObject from a JsonSchema, with no OpenAPI-specific
+   * vocabulary.
+   */
+  def fromJsonSchema(js: zio.blocks.schema.json.JsonSchema): SchemaObject =
+    SchemaObject(jsonSchema = js.toJson)
+
+  implicit val schema: Schema[SchemaObject] = Schema.derived
+}
+
+/**
+ * The location of the parameter.
+ *
+ * Possible values are "query", "header", "path" or "cookie".
+ */
+sealed trait ParameterLocation
+
+object ParameterLocation {
+  case object Query  extends ParameterLocation
+  case object Header extends ParameterLocation
+  case object Path   extends ParameterLocation
+  case object Cookie extends ParameterLocation
+
+  implicit val schema: Schema[ParameterLocation] = Schema.derived
+}
+
+/**
+ * Describes a single operation parameter.
+ *
+ * A unique parameter is defined by a combination of a name and location.
+ *
+ * @param name
+ *   REQUIRED. The name of the parameter. Parameter names are case sensitive.
+ * @param in
+ *   REQUIRED. The location of the parameter.
+ * @param description
+ *   A brief description of the parameter. This could contain examples of use.
+ *   CommonMark syntax MAY be used for rich text representation.
+ * @param required
+ *   Determines whether this parameter is mandatory. If the parameter location
+ *   is "path", this property is REQUIRED and its value MUST be true. Otherwise,
+ *   the property MAY be included and its default value is false.
+ * @param deprecated
+ *   Specifies that a parameter is deprecated and SHOULD be transitioned out of
+ *   usage. Default value is false.
+ * @param allowEmptyValue
+ *   Sets the ability to pass empty-valued parameters. This is valid only for
+ *   query parameters and allows sending a parameter with an empty value.
+ *   Default value is false.
+ * @param style
+ *   Describes how the parameter value will be serialized depending on the type
+ *   of the parameter value.
+ * @param explode
+ *   When this is true, parameter values of type array or object generate
+ *   separate parameters for each value of the array or key-value pair of the
+ *   map.
+ * @param allowReserved
+ *   Determines whether the parameter value SHOULD allow reserved characters, as
+ *   defined by RFC3986, to be included without percent-encoding.
+ * @param schema
+ *   The schema defining the type used for the parameter.
+ * @param example
+ *   Example of the parameter's potential value. The example SHOULD match the
+ *   specified schema and encoding properties if present.
+ * @param examples
+ *   Examples of the parameter's potential value. Each example SHOULD contain a
+ *   value in the correct format as specified in the parameter encoding.
+ * @param content
+ *   A map containing the representations for the parameter. The key is the
+ *   media type and the value describes it.
+ * @param extensions
+ *   Specification extensions (x-* fields). These allow adding additional
+ *   properties beyond the standard OpenAPI fields.
+ */
+final case class Parameter private (
+  name: String,
+  in: ParameterLocation,
+  description: Option[String] = None,
+  required: Boolean = false,
+  deprecated: Boolean = false,
+  allowEmptyValue: Boolean = false,
+  style: Option[String] = None,
+  explode: Option[Boolean] = None,
+  allowReserved: Option[Boolean] = None,
+  schema: Option[Json] = None,
+  example: Option[Json] = None,
+  examples: Map[String, Json] = Map.empty,
+  content: Map[String, Json] = Map.empty,
+  extensions: Map[String, Json] = Map.empty
+) {
+  require(
+    in != ParameterLocation.Path || required,
+    "Parameter with location 'path' must have required=true"
+  )
+}
+
+object Parameter {
+  implicit val schema: Schema[Parameter] = Schema.derived
+
+  /**
+   * Creates a Parameter with validation of the required field constraint.
+   *
+   * If the parameter location is "path", the required field MUST be true.
+   */
+  def apply(
+    name: String,
+    in: ParameterLocation,
+    description: Option[String] = None,
+    required: Boolean = false,
+    deprecated: Boolean = false,
+    allowEmptyValue: Boolean = false,
+    style: Option[String] = None,
+    explode: Option[Boolean] = None,
+    allowReserved: Option[Boolean] = None,
+    schema: Option[Json] = None,
+    example: Option[Json] = None,
+    examples: Map[String, Json] = Map.empty,
+    content: Map[String, Json] = Map.empty,
+    extensions: Map[String, Json] = Map.empty
+  ): Parameter = {
+    require(
+      in != ParameterLocation.Path || required,
+      "Parameter with location 'path' must have required=true"
+    )
+    new Parameter(
+      name,
+      in,
+      description,
+      required,
+      deprecated,
+      allowEmptyValue,
+      style,
+      explode,
+      allowReserved,
+      schema,
+      example,
+      examples,
+      content,
+      extensions
+    )
+  }
+}
+
+/**
+ * The Header Object follows the structure of the Parameter Object with the
+ * following changes:
+ *
+ *   1. name MUST NOT be specified, it is given in the corresponding headers
+ *      map.
+ *   2. in MUST NOT be specified, it is implicitly in header.
+ *   3. All traits that are affected by the location MUST be applicable to a
+ *      location of header (for example, style).
+ *
+ * @param description
+ *   A brief description of the header. This could contain examples of use.
+ *   CommonMark syntax MAY be used for rich text representation.
+ * @param required
+ *   Determines whether this header is mandatory. Default value is false.
+ * @param deprecated
+ *   Specifies that a header is deprecated and SHOULD be transitioned out of
+ *   usage. Default value is false.
+ * @param allowEmptyValue
+ *   Sets the ability to pass empty-valued headers. Default value is false.
+ * @param style
+ *   Describes how the header value will be serialized.
+ * @param explode
+ *   When this is true, header values of type array or object generate separate
+ *   parameters for each value of the array or key-value pair of the map.
+ * @param allowReserved
+ *   Determines whether the header value SHOULD allow reserved characters, as
+ *   defined by RFC3986, to be included without percent-encoding.
+ * @param schema
+ *   The schema defining the type used for the header.
+ * @param example
+ *   Example of the header's potential value.
+ * @param examples
+ *   Examples of the header's potential value.
+ * @param content
+ *   A map containing the representations for the header. The key is the media
+ *   type and the value describes it.
+ * @param extensions
+ *   Specification extensions (x-* fields). These allow adding additional
+ *   properties beyond the standard OpenAPI fields.
+ */
+final case class Header(
+  description: Option[String] = None,
+  required: Boolean = false,
+  deprecated: Boolean = false,
+  allowEmptyValue: Boolean = false,
+  style: Option[String] = None,
+  explode: Option[Boolean] = None,
+  allowReserved: Option[Boolean] = None,
+  schema: Option[Json] = None,
+  example: Option[Json] = None,
+  examples: Map[String, Json] = Map.empty,
+  content: Map[String, Json] = Map.empty,
+  extensions: Map[String, Json] = Map.empty
+)
+
+object Header {
+  implicit val schema: Schema[Header] = Schema.derived
 }
