db8bdc79ec71aa1f62f7d919ce1a7cdba4d5f1f6
mccoychang <mccoychang@gmail.com>
2026-02-08 16:15:31 +0800

    fix(ci): scalafmt format scala-2.12-2.13 MacroMigration

--- schema/shared/src/main/scala-2.12-2.13/zio/blocks/schema/migration/macros/MacroMigration.scala
+++ schema/shared/src/main/scala-2.12-2.13/zio/blocks/schema/migration/macros/MacroMigration.scala
@@ -3,9 +3,10 @@ package zio.blocks.schema.migration.macros
 import zio.blocks.schema.migration.{DynamicMigration, Migration}
 
 object MacroMigration {
+
   /**
-   * Scala 2 Fallback: Always uses the interpreter.
-   * Zero-Overhead macros are only available on Scala 3.
+   * Scala 2 Fallback: Always uses the interpreter. Zero-Overhead macros are
+   * only available on Scala 3.
    */
   def derive[A, B](dynamic: DynamicMigration): Migration[A, B] =
     Migration.manual[A, B](dynamic)
