f50d5d9118e254350e50c14504f6261193ceb634
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2026-02-11 17:06:18 +0100

    More efficient implementation of `toJsonString` syntax

--- schema/shared/src/main/scala-2/zio/blocks/schema/syntax.scala
+++ schema/shared/src/main/scala-2/zio/blocks/schema/syntax.scala
@@ -4,27 +4,27 @@ import zio.blocks.schema.json.{Json, JsonEncoder, JsonFormat}
 import zio.blocks.schema.patch.{Patch, PatchMode}
 
 trait SyntaxVersionSpecific {
-  implicit final class SchemaValueOps[A](private val self: A) {
+  implicit final class SchemaValueOps[A](self: A) {
+    def applyPatch(patch: Patch[A]): A = patch(self)
+
+    def applyPatchStrict(patch: Patch[A]): Either[SchemaError, A] = patch(self, PatchMode.Strict)
+
     def diff(that: A)(implicit schema: Schema[A]): Patch[A] = schema.diff(self, that)
 
     def show(implicit schema: Schema[A]): String = schema.toDynamicValue(self).toString
 
     def toJson(implicit jsonEncoder: JsonEncoder[A]): Json = jsonEncoder.encode(self)
 
-    def toJsonString(implicit schema: Schema[A]): String = toJson.print
+    def toJsonString(implicit schema: Schema[A]): String = schema.getInstance(JsonFormat).encodeToString(self)
 
     def toJsonBytes(implicit schema: Schema[A]): Array[Byte] = schema.getInstance(JsonFormat).encode(self)
-
-    def applyPatch(patch: Patch[A]): A = patch(self)
-
-    def applyPatchStrict(patch: Patch[A]): Either[SchemaError, A] = patch(self, PatchMode.Strict)
   }
 
-  implicit final class StringSchemaOps(private val self: String) {
+  implicit final class StringSchemaOps(self: String) {
     def fromJson[A](implicit schema: Schema[A]): Either[SchemaError, A] = schema.getInstance(JsonFormat).decode(self)
   }
 
-  implicit final class ByteArraySchemaOps(private val self: Array[Byte]) {
+  implicit final class ByteArraySchemaOps(self: Array[Byte]) {
     def fromJson[A](implicit schema: Schema[A]): Either[SchemaError, A] = schema.getInstance(JsonFormat).decode(self)
   }
 }
