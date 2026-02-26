e98d177677daa9f27dbd38ba68da6f3e30aeb842
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2026-02-26 21:23:20 +0100

    test(schema-csv): add CsvConfig and CsvError specs

---
+++ schema-csv/src/test/scala/zio/blocks/schema/csv/CsvConfigSpec.scala
@@ -0,0 +1,83 @@
+package zio.blocks.schema.csv
+
+import zio.blocks.schema.SchemaBaseSpec
+import zio.test._
+
+object CsvConfigSpec extends SchemaBaseSpec {
+  def spec = suite("CsvConfigSpec")(
+    suite("default CsvConfig")(
+      test("creates default config with RFC 4180 standards") {
+        val config = CsvConfig.default
+        assertTrue(
+          config.delimiter == ',' &&
+            config.quoteChar == '"' &&
+            config.escapeChar == '"' &&
+            config.lineTerminator == "\r\n" &&
+            config.hasHeader == true &&
+            config.nullValue == ""
+        )
+      },
+      test("CsvConfig() uses defaults") {
+        val config = CsvConfig()
+        assertTrue(config == CsvConfig.default)
+      }
+    ),
+    suite("custom CsvConfig")(
+      test("can create config with custom delimiter") {
+        val config = CsvConfig(delimiter = ';')
+        assertTrue(config.delimiter == ';' && config.quoteChar == '"')
+      },
+      test("can create config with custom quoteChar") {
+        val config = CsvConfig(quoteChar = '\'')
+        assertTrue(config.quoteChar == '\'' && config.delimiter == ',')
+      },
+      test("can create config with custom lineTerminator") {
+        val config = CsvConfig(lineTerminator = "\n")
+        assertTrue(config.lineTerminator == "\n")
+      },
+      test("can create config with hasHeader = false") {
+        val config = CsvConfig(hasHeader = false)
+        assertTrue(config.hasHeader == false && config.hasHeader != CsvConfig.default.hasHeader)
+      },
+      test("can create config with custom nullValue") {
+        val config = CsvConfig(nullValue = "N/A")
+        assertTrue(config.nullValue == "N/A")
+      }
+    ),
+    suite("TSV preset")(
+      test("CsvConfig.tsv uses tab delimiter") {
+        assertTrue(CsvConfig.tsv.delimiter == '\t')
+      },
+      test("CsvConfig.tsv preserves other defaults") {
+        val tsv = CsvConfig.tsv
+        assertTrue(
+          tsv.quoteChar == '"' &&
+            tsv.escapeChar == '"' &&
+            tsv.lineTerminator == "\r\n" &&
+            tsv.hasHeader == true &&
+            tsv.nullValue == ""
+        )
+      },
+      test("CsvConfig.tsv is different from default") {
+        assertTrue(CsvConfig.tsv.delimiter != CsvConfig.default.delimiter)
+      }
+    ),
+    suite("case class behavior")(
+      test("CsvConfig equality works") {
+        val config1 = CsvConfig(delimiter = ';')
+        val config2 = CsvConfig(delimiter = ';')
+        assertTrue(config1 == config2)
+      },
+      test("CsvConfig inequality works") {
+        val config1 = CsvConfig(delimiter = ';')
+        val config2 = CsvConfig(delimiter = ',')
+        assertTrue(config1 != config2)
+      },
+      test("CsvConfig copy works") {
+        val config1 = CsvConfig.default
+        val config2 = config1.copy(delimiter = '\t', hasHeader = false)
+        assertTrue(config2.delimiter == '\t' && config2.hasHeader == false && config2.quoteChar == '"')
+      }
+    )
+  )
+}
