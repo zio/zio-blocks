ca4bd2ee59124089070fdd97fd8b8414d59490fc
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2026-02-26 02:53:16 +0100

    fix(schema): add Derivable type class for unified schema.derive method (#1023) (#1032)

--- schema/shared/src/test/scala-2/zio/blocks/schema/SchemaVersionSpecificSpec.scala
+++ schema/shared/src/test/scala-2/zio/blocks/schema/SchemaVersionSpecificSpec.scala
@@ -5,6 +5,51 @@ import zio.test._
 
 object SchemaVersionSpecificSpec extends SchemaBaseSpec {
   def spec: Spec[TestEnvironment, Any] = suite("SchemaVersionSpecificSpec")(
+    test("compiles when explicit type annotation is used with schema.derive(Format)") {
+      typeCheck {
+        """
+        import zio.blocks.schema._
+        import zio.blocks.schema.json.{JsonBinaryCodec, JsonFormat}
+
+        case class Person(name: String, age: Int)
+
+        object Person {
+          implicit val schema: Schema[Person] = Schema.derived[Person]
+          implicit val jsonCodec: JsonBinaryCodec[Person] = schema.derive(JsonFormat)
+        }
+        """
+      }.map(result => assert(result)(isRight))
+    },
+    test("compiles when explicit type annotation is used with schema.derive(deriver)") {
+      typeCheck {
+        """
+        import zio.blocks.schema._
+        import zio.blocks.schema.json.{JsonBinaryCodec, JsonBinaryCodecDeriver}
+
+        case class Person(name: String, age: Int)
+
+        object Person {
+          implicit val schema: Schema[Person] = Schema.derived[Person]
+          implicit val jsonCodec: JsonBinaryCodec[Person] = schema.derive(JsonBinaryCodecDeriver)
+        }
+        """
+      }.map(result => assert(result)(isRight))
+    },
+    test("compiles without explicit type annotation using schema.derive(Format)") {
+      typeCheck {
+        """
+        import zio.blocks.schema._
+        import zio.blocks.schema.json.JsonFormat
+
+        case class Person(name: String, age: Int)
+
+        object Person {
+          implicit val schema: Schema[Person] = Schema.derived[Person]
+          val jsonCodec = schema.derive(JsonFormat)
+        }
+        """
+      }.map(result => assert(result)(isRight))
+    },
     test("doesn't generate schema for unsupported classes") {
       typeCheck {
         "Schema.derived[scala.concurrent.duration.Duration]"
