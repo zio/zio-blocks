9479bb51bd7e1e8be9f91feb025a58db9f678c2f
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2026-02-27 22:30:32 +0100

    feat(codegen): add project scaffolding and build.sbt module definition

---
+++ codegen/src/main/scala/zio/blocks/codegen/package.scala
@@ -0,0 +1,10 @@
+/**
+ * Codegen module providing a generic IR and emitter for Scala code generation.
+ *
+ * This module defines:
+ *   - `ir`: Intermediate representation for code generation
+ *   - `emit`: Code emitters for various target languages
+ */
+package zio.blocks
+
+package object codegen
