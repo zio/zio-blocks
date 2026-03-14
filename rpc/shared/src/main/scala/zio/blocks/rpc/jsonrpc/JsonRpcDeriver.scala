a13ed0a6794bdf42db3e6fc2acf9cc0c633ead7d
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2026-03-14 14:40:19 +0100

    feat(rpc): add zio-blocks-rpc module with derives RPC type class
    Adds a new zio-blocks-rpc module providing a derives RPC type class that captures
    service trait structure as a pure data representation, analogous to how derives
    Schema works for data types. Includes JSON-RPC 2.0 reference protocol.
    
    - Guard against >22 params with clear compile error
    - Validate JSON-RPC id type per spec (String/Number/Null only)
    - Document notification limitation
    
    Closes #1143

---
+++ rpc/shared/src/main/scala/zio/blocks/rpc/jsonrpc/JsonRpcDeriver.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024-2026 John A. De Goes and the ZIO Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zio.blocks.rpc.jsonrpc
+
+import zio._
+import zio.blocks.rpc._
+import zio.blocks.schema.json.Json
+
+/**
+ * RpcDeriver that produces JsonRpcCodec instances from RPC descriptors.
+ */
+object JsonRpcDeriver extends RpcDeriver[JsonRpcCodec] {
+
+  override def deriveService[T](rpc: RPC[T]): JsonRpcCodec[T] = {
+    val handlers = rpc.operations.toList.map { op =>
+      op.name -> createHandler(op)
+    }.toMap
+    new JsonRpcCodec[T](handlers)
+  }
+
+  private def createHandler(op: RPC.Operation[?, ?]): JsonRpcCodec.OperationHandler =
+    new JsonRpcCodec.OperationHandler {
+      def handle(params: Json): ZIO[Any, Throwable, Json] =
+        ZIO.fail(
+          new UnsupportedOperationException(
+            s"Operation '${op.name}' handler not bound to a service implementation"
+          )
+        )
+    }
+}
