e2b91d78d7999d823b3856a459244bfd6fdd38df
Avinder Bahra <avinder.bahra@gmail.com>
2026-03-28 08:54:12 +0000

    fix package statements

--- schema/shared/src/test/scala-2/zio/blocks/schema/comptime/AllowsReproducerOk.scala
+++ schema/shared/src/test/scala-2/zio/blocks/schema/comptime/AllowsReproducerOk.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package foo
+package zio.blocks.schema.comptime
 
 object AllowsReproducerOk extends App {
   import zio.blocks.schema.Schema
