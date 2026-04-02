3c5180a3dca685e866808897e3a6e8a8a2178e68
Chronolapse411 <chronolapse411@gmail.com>
2026-04-02 02:51:02 -0400

    Fix DynamicMigration constructor by wrapping MigrationAction in Vector

--- schema/shared/src/main/scala/zio/blocks/schema/migration/MigrationBuilder.scala
+++ schema/shared/src/main/scala/zio/blocks/schema/migration/MigrationBuilder.scala
@@ -9,25 +9,25 @@ class MigrationBuilder[Source, Target, Current](
 ) {
   // Methods for building migrations dynamically
   def addField[F](at: DynamicOptic, default: SchemaExpr[Any, Any]): MigrationBuilder[Source, Target, Current] =
-    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(MigrationAction.AddField(at, default)))
+    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(Vector(MigrationAction.AddField(at, default))))
 
   def dropField(at: DynamicOptic): MigrationBuilder[Source, Target, Current] =
-    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(MigrationAction.DropField(at, SchemaExpr.DefaultValue(Schema.dynamicValue))))
+    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(Vector(MigrationAction.DropField(at, SchemaExpr.DefaultValue(Schema.dynamicValue)))))
 
   def rename(at: DynamicOptic, to: String): MigrationBuilder[Source, Target, Current] =
-    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(MigrationAction.Rename(at, to)))
+    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(Vector(MigrationAction.Rename(at, to))))
 
   def mandate(at: DynamicOptic, default: SchemaExpr[Any, Any]): MigrationBuilder[Source, Target, Current] =
-    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(MigrationAction.Mandate(at, default)))
+    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(Vector(MigrationAction.Mandate(at, default))))
 
   def optionalize(at: DynamicOptic): MigrationBuilder[Source, Target, Current] =
-    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(MigrationAction.Optionalize(at)))
+    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(Vector(MigrationAction.Optionalize(at))))
 
   def renameCase(at: DynamicOptic, from: String, to: String): MigrationBuilder[Source, Target, Current] =
-    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(MigrationAction.RenameCase(at, from, to)))
+    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(Vector(MigrationAction.RenameCase(at, from, to))))
 
   def transformCase(at: DynamicOptic, actions: DynamicMigration): MigrationBuilder[Source, Target, Current] =
-    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(MigrationAction.TransformCase(at, actions.actions)))
+    new MigrationBuilder(sourceSchema, targetSchema, dynamicMigration ++ DynamicMigration(Vector(MigrationAction.TransformCase(at, actions.actions))))
 
   def buildPartial: Migration[Source, Target] = Migration(sourceSchema, targetSchema, dynamicMigration)
 }
