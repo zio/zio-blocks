b29ba7f628587f5a13e420cd8dd9f0d5836b06fc
Eliene-byte <elienedantas123456@gmail.com>
2026-04-11 14:15:56 -0300

    feat(schema): implement DynamicMigration.apply with Record path handling and tests for #519

--- shared/src/main/scala/zio/blocks/schema/migration/Migration.scala
+++ shared/src/main/scala/zio/blocks/schema/migration/Migration.scala
@@ -7,13 +7,19 @@ final case class Migration[A, B](
     schemaA: Schema[A],
     schemaB: Schema[B]
 ) {
-  def apply(value: A): Either[MigrationError, B] = Left(MigrationError("not implemented"))
+  def apply(value: A): Either[MigrationError, B] =
+    toDynamic(schemaA.toDynamic(value))
+     .flatMap(_.toTypedValue(schemaB).left.map(e => MigrationError(e.toString)))
+
   def ++[C](that: Migration[B, C]): Migration[A, C] =
     Migration(toDynamic ++ that.toDynamic, schemaA, that.schemaB)
+
   def andThen[C](that: Migration[B, C]): Migration[A, C] = this ++ that
   def reverse: Migration[B, A] = Migration(toDynamic.reverse, schemaB, schemaA)
 }
 object Migration {
-  def newBuilder[A, B](using Schema[A], Schema[B]): MigrationBuilder[A, B] = new MigrationBuilder[A, B]
-  def identity[A](using Schema[A]): Migration[A, A] = Migration(DynamicMigration.empty, summon, summon)
+  def newBuilder[A, B](using Schema[A], Schema[B]): MigrationBuilder[A, B] =
+    new MigrationBuilder[A, B]
+  def identity[A](using Schema[A]): Migration[A, A] =
+    Migration(DynamicMigration.empty, summon[Schema[A]], summon[Schema[A]])
 }
